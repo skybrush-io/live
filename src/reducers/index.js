63af6bfda00787ff249ee9b6437da58b953a0df1
Tamas Nepusz <ntamas@gmail.com>
2016-05-07 23:29:06 +0200

    parts of the store are now persisted in browser.localStorage

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,21 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
+import * as storage from 'redux-storage'
 
 import connectionsReducer from './connections'
 import serverSettingsReducer from './server-settings'
 import snackbarReducer from './snackbar'
 
-const reducer = combineReducers({
+/**
+ * The global reducer of the application, wrapped in a wrapper provided by
+ * <code>redux-storage</code> to facilitate saving its state in the local
+ * storage of the browser.
+ */
+const reducer = storage.reducer(combineReducers({
   connections: connectionsReducer,
   serverSettings: serverSettingsReducer,
   snackbar: snackbarReducer,
   form: formReducer
-})
+}))
 
 export default reducer
--- src/store.js
+++ src/store.js
@@ -2,14 +2,44 @@
  * @file The master store for the application state.
  */
 
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import * as storage from 'redux-storage'
+import filter from 'redux-storage-decorator-filter'
+import createEngine from 'redux-storage-engine-localstorage'
+
 import reducer from './reducers'
+
+/**
+ * Storage engine for storing the application state in the browser's
+ * local storage.
+ */
+const engine = filter(
+  createEngine('flockwave-client'), [
+    'whitelisted-key',
+    ['serverSettings']
+  ]
+)
+
+/**
+ * Redux middleware that saves the state of the application into the
+ * browser's local storage after every action.
+ */
+const storageMiddleware = storage.createMiddleware(engine)
+
+/**
+ * Function to create a new Redux store with the required middlewares.
+ */
+const createStoreWithMiddleware = applyMiddleware(storageMiddleware)(createStore)
 
 /**
  * The store for the application state.
  */
-const store = createStore(
+const store = createStoreWithMiddleware(
     reducer, undefined,
     window.devToolsExtension ? window.devToolsExtension() : undefined
 )
+
+// Restore any previous state of the store
+storage.createLoader(engine)(store)
+
 export default store
