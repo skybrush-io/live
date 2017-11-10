7ca218c7c4653401f85b090f0de8334dbf935019
Tamas Nepusz <ntamas@gmail.com>
2017-11-10 23:32:01 +0100

    feature layer can now render paths and polygons

--- src/components/map/layers/features.jsx
+++ src/components/map/layers/features.jsx
@@ -1,3 +1,4 @@
+import Color from 'color'
 import { Feature, geom, layer, source } from 'ol-react'
 import PropTypes from 'prop-types'
 import React from 'react'
@@ -23,20 +24,49 @@ export const FeaturesLayerSettings = connect(
   (dispatch, ownProps) => ({})
 )(FeaturesLayerSettingsPresentation)
 
+// === Helper functions ===
+
+const geometryForFeature = feature => {
+  const { points, type } = feature
+  const coordinates = points.map(point => coordinateFromLonLat([point.lon, point.lat]))
+
+  switch (type) {
+    // TODO: points and point sets
+    case 'path': return <geom.LineString>{coordinates}</geom.LineString>
+    case 'polygon': return <geom.Polygon>{coordinates}</geom.Polygon>
+    default: return null
+  }
+}
+
+const styleForFeature = feature => {
+  const parsedColor = Color(feature.color || '#0088ff')
+  console.log(parsedColor.rgb().array())
+  return ({
+    stroke: {
+      color: parsedColor.rgb().array(),
+      width: 2
+    },
+    fill: {
+      color: parsedColor.fade(0.5).rgb().array()
+    }
+  })
+}
+
+const renderFeature = feature => {
+  const { id } = feature
+  return (
+    <Feature id={id} key={id} style={styleForFeature(feature)}>
+      {geometryForFeature(feature)}
+    </Feature>
+  )
+}
+
 // === The actual layer to be rendered ===
 
 const FeaturesLayerPresentation = ({ features, projection, zIndex }) => (
   <layer.Vector updateWhileAnimating updateWhileInteracting zIndex={zIndex}>
     <source.Vector>
-      {features && features.map(({ id, points }) => (
-        <Feature id={id} key={id}>
-          <geom.LineString>
-            {
-              points.map(point => coordinateFromLonLat([point.lon, point.lat]))
-            }
-          </geom.LineString>
-        </Feature>
-      ))}
+      {features && features.map(renderFeature)}
     </source.Vector>
   </layer.Vector>
 )
