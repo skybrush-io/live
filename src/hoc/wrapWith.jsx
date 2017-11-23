560f84a2b154abcaa4f9defc91f9585cf3c0c024
Tamas Nepusz <ntamas@gmail.com>
2017-11-23 21:55:15 +0100

    fixed map resizing when its container is resized

--- src/hoc/wrapWith.jsx
+++ src/hoc/wrapWith.jsx
@@ -1,15 +1,19 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 import { getDisplayName, wrapDisplayName } from 'recompose'
 
 const wrapWith = WrapperComponent =>
   BaseComponent => {
-    const result = (children, ...restProps) => (
+    const result = ({ children, ...restProps }) => (
       <WrapperComponent>
         <BaseComponent {...restProps}>
           {children}
         </BaseComponent>
       </WrapperComponent>
     )
+    result.propTypes = {
+      children: PropTypes.node
+    }
     result.displayName = wrapDisplayName(BaseComponent,
       'wrapWith[' + getDisplayName(WrapperComponent) + ']'
     )
