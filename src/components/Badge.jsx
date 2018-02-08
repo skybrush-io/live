0a473b1068243b1050a9614a05dd1d99e9ae3895
Tamas Nepusz <ntamas@gmail.com>
2018-02-08 15:42:54 +0100

    added connection status indicator

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -3,18 +3,35 @@ import * as React from 'react'
 
 import Transition from 'react-transition-group/Transition'
 
-const renderBadge = state => (
-  <div className='wb-modification-indicator' style={{
-    transform: (state === 'entering' || state === 'entered') ? 'scale(1)' : 'scale(0)',
-    transition: 'transform 300ms ease-in-out'
-  }} />
-)
+export const Badge = ({ color, offset, visible }) => {
+  const baseStyles = {}
 
-export const Badge = ({ color, visible }) => (
-  <Transition timeout={300} in={visible}>{renderBadge}</Transition>
-)
+  if (color) {
+    baseStyles.background = color
+  }
+
+  if (offset) {
+    baseStyles.top = offset[1]
+    baseStyles.right = offset[0]
+  }
+
+  return (
+    <Transition appear timeout={300} in={visible}>
+      {
+        state => (
+          <div className='wb-modification-indicator' style={{
+            ...baseStyles,
+            transform: (state === 'entering' || state === 'entered') ? 'scale(1)' : 'scale(0)',
+            transition: 'transform 300ms ease-in-out, background-color 300ms linear'
+          }} />
+        )
+      }
+    </Transition>
+  )
+}
 
 Badge.propTypes = {
   color: PropTypes.string,
+  offset: PropTypes.arrayOf(PropTypes.number),
   visible: PropTypes.bool
 }
--- src/components/ConnectionSettingsButton.jsx
+++ src/components/ConnectionSettingsButton.jsx
@@ -8,13 +8,21 @@ import { connect } from 'react-redux'
 import { Badge } from './Badge'
 
 import { showServerSettingsDialog } from '../actions/server-settings'
+import { ConnectionState, MASTER_CONNECTION_ID } from '../model/connections'
 
-const ConnectionSettingsButtonPresentation = ({ isConnected, onClick }) => {
+const badgeColorForState = {
+  [ConnectionState.CONNECTED]: '#0c0',
+  [ConnectionState.CONNECTING]: '#fc0',
+  [ConnectionState.DISCONNECTING]: '#fc0'
+}
+
+const ConnectionSettingsButtonPresentation = ({ onClick, state }) => {
   const classes = ['wb-module']
   return (
     <div className={classes.join(' ')} onClick={onClick}>
-      <span className='wb-icon wb-module-icon'>
-        <Badge visible />
+      <span className='wb-icon wb-module-icon' style={{ position: 'relative' }}>
+        <Badge visible={state !== ConnectionState.DISCONNECTED}
+          color={badgeColorForState[state]} offset={[8, 8]} />
         <ConnectionIcon onClick={onClick} />
       </span>
       Connection
@@ -23,14 +31,14 @@ const ConnectionSettingsButtonPresentation = ({ isConnected, onClick }) => {
 }
 
 ConnectionSettingsButtonPresentation.propTypes = {
-  isConnected: PropTypes.bool,
-  onClick: PropTypes.func
+  onClick: PropTypes.func,
+  state: PropTypes.string
 }
 
 export default connect(
   // mapStateToProps
   state => ({
-    isConnected: true
+    state: state.connections.byId[MASTER_CONNECTION_ID].state
   }),
   // mapDispatchToProps
   dispatch => ({
