7c75138dceff395e6db6cfcf0b1124ec50daefb5
Tamas Nepusz <ntamas@gmail.com>
2019-12-31 02:55:15 +0100

    fixed merging of stored state with default state

--- src/store.js
+++ src/store.js
@@ -9,7 +9,9 @@ import {
 } from '@reduxjs/toolkit';
 
 import localForage from 'localforage';
+import has from 'lodash-es/has';
 import isFunction from 'lodash-es/isFunction';
+import pickBy from 'lodash-es/pickBy';
 import createDeferred from 'p-defer';
 import createDebounce from 'redux-debounce';
 import promise from 'redux-promise-middleware';
@@ -35,7 +37,43 @@ const persistConfig = {
   version: 1,
 
   // reconcile two levels of the incoming state object with the defaults
-  stateReconciler: autoMergeLevel2,
+  stateReconciler: (inboundState, originalState, reducedState, ...rest) => {
+    // Iterate over the top-level collections in the inboundState. If the
+    // collections are defined, don't merge them with the collections in the
+    // originalState as we don't want the example objects that we load into
+    // the store by default to "come back" every time the state is rehydrated
+    // at application startup.
+
+    const isCollection = stateSlice => stateSlice && has(stateSlice, 'byId');
+    const isSubslice = stateSlice =>
+      stateSlice !== null &&
+      typeof stateSlice === 'object' &&
+      !Array.isArray(stateSlice);
+
+    const cleanCollections = (inbound, original) => {
+      const cleaned = pickBy(original, (_, key) => !isCollection(inbound[key]));
+
+      for (const key of Object.keys(cleaned)) {
+        if (isSubslice(cleaned[key]) && has(inbound, key)) {
+          cleaned[key] = cleanCollections(inbound[key], cleaned[key]);
+        }
+      }
+
+      return cleaned;
+    };
+
+    if (inboundState && typeof inboundState === 'object') {
+      // Maybe we have a hack here: originalState is outright ignored and
+      // we pass cleanedState both as original and as "reduced" state to
+      // autoMergeLevel2(). It is unclear to me what the difference is between
+      // the two states, and it does not work if I pass originalState instead
+      // of cleanedState in the second argument.
+      const cleanedState = cleanCollections(inboundState, reducedState);
+      return autoMergeLevel2(inboundState, cleanedState, cleanedState, ...rest);
+    }
+
+    return autoMergeLevel2(inboundState, originalState, reducedState, ...rest);
+  },
 
   // do not store the following slices of the state in the storage
   blacklist: [
