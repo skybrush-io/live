e4e972596ce1e5f5335ef8ab1e79d11e28b9b8ac
Tamas Nepusz <ntamas@gmail.com>
2020-02-02 23:21:29 +0100

    tweaks

--- src/views/uavs/MappingSlotEditorToolbar.jsx
+++ src/views/uavs/MappingSlotEditorToolbar.jsx
@@ -3,25 +3,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import Box from '@material-ui/core/Box';
-import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
 import Toolbar from '@material-ui/core/Toolbar';
 import Check from '@material-ui/icons/Check';
+import Close from '@material-ui/icons/Close';
 import Mouse from '@material-ui/icons/Mouse';
-import MoreVert from '@material-ui/icons/MoreVert';
 
 import {
-  clearMapping,
+  cancelMappingEditorSessionAtCurrentSlot,
   finishMappingEditorSession
 } from '~/features/mission/slice';
-import useDropdown from '~/hooks/useDropdown';
 
 const MappingSlotEditorToolbar = React.forwardRef(
-  ({ clearMapping, finishMappingEditorSession, ...rest }, ref) => {
-    const [menuAnchorEl, openMappingMenu, closeMappingMenu] = useDropdown();
-
+  (
+    {
+      cancelMappingEditorSessionAtCurrentSlot,
+      finishMappingEditorSession,
+      ...rest
+    },
+    ref
+  ) => {
     return (
       <Toolbar ref={ref} disableGutters variant="dense" {...rest}>
         <IconButton disabled>
@@ -32,30 +33,19 @@ const MappingSlotEditorToolbar = React.forwardRef(
           direction.
         </Box>
         <Box flex={1} />
+        <IconButton onClick={cancelMappingEditorSessionAtCurrentSlot}>
+          <Close />
+        </IconButton>
         <IconButton onClick={finishMappingEditorSession}>
           <Check />
         </IconButton>
-        <IconButton onClick={openMappingMenu}>
-          <MoreVert />
-        </IconButton>
-        <Menu
-          anchorEl={menuAnchorEl}
-          open={menuAnchorEl !== null}
-          variant="menu"
-          onClose={closeMappingMenu}
-        >
-          <MenuItem disabled>Import...</MenuItem>
-          <MenuItem disabled>Export...</MenuItem>
-          <Divider />
-          <MenuItem onClick={closeMappingMenu(clearMapping)}>Clear</MenuItem>
-        </Menu>
       </Toolbar>
     );
   }
 );
 
 MappingSlotEditorToolbar.propTypes = {
-  clearMapping: PropTypes.func,
+  cancelMappingEditorSessionAtCurrentSlot: PropTypes.func,
   finishMappingEditorSession: PropTypes.func,
   selectedUAVIds: PropTypes.array
 };
@@ -64,7 +54,7 @@ export default connect(
   // mapStateToProps
   null,
   // mapDispatchToProps
-  { clearMapping, finishMappingEditorSession },
+  { cancelMappingEditorSessionAtCurrentSlot, finishMappingEditorSession },
   null,
   { forwardRef: true }
 )(MappingSlotEditorToolbar);
