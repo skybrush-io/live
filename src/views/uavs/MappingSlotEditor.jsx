ae9ec564d89536dfda92d0f8ffdb56de6f8c9463
Tamas Nepusz <ntamas@gmail.com>
2020-02-03 14:57:42 +0100

    fixed styling of <kbd> tags in dark mode

--- src/views/uavs/MappingSlotEditor.jsx
+++ src/views/uavs/MappingSlotEditor.jsx
@@ -56,6 +56,7 @@ const MappingSlotEditor = ({
   const classes = useStyles();
 
   const onBlur = event => {
+    return;
     commitMappingEditorSessionAtCurrentSlot({
       value: event.target.value
     });
--- src/views/uavs/MappingSlotEditorToolbar.jsx
+++ src/views/uavs/MappingSlotEditorToolbar.jsx
@@ -3,23 +3,48 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import Box from '@material-ui/core/Box';
+import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles } from '@material-ui/core/styles';
 import Close from '@material-ui/icons/Close';
-import Mouse from '@material-ui/icons/Mouse';
+import Keyboard from '@material-ui/icons/Keyboard';
 
 import { cancelMappingEditorSessionAtCurrentSlot } from '~/features/mission/slice';
 
+const useStyles = makeStyles(
+  theme => ({
+    box: {
+      userSelect: 'none',
+      whiteSpace: 'nowrap'
+    },
+    divider: {
+      alignSelf: 'stretch',
+      height: 'auto',
+      margin: theme.spacing(1, 1)
+    }
+  }),
+  { name: 'MappingSlotEditorToolbar' }
+);
+
 const MappingSlotEditorToolbar = React.forwardRef(
   ({ cancelMappingEditorSessionAtCurrentSlot, ...rest }, ref) => {
+    const classes = useStyles();
     return (
       <Toolbar ref={ref} disableGutters variant="dense" {...rest}>
         <IconButton disabled>
-          <Mouse />
+          <Keyboard />
         </IconButton>
-        <Box style={{ userSelect: 'none' }}>
-          Enter to save. Tab to move to the next empty slot. Shift reverses
-          direction.
+        <Box className={classes.box}>
+          <kbd>Enter</kbd> Save
+        </Box>
+        <Divider orientation="vertical" className={classes.divider} />
+        <Box className={classes.box}>
+          <kbd>Tab</kbd> Select next empty slot
+        </Box>
+        <Divider orientation="vertical" className={classes.divider} />
+        <Box className={classes.box}>
+          Hold <kbd>Shift</kbd> to reverse direction.
         </Box>
         <Box flex={1} />
         <IconButton onClick={cancelMappingEditorSessionAtCurrentSlot}>
