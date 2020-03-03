9c4ccead344c19a3aecf47859ee0c1c51908d70c
Tamas Nepusz <ntamas@gmail.com>
2020-03-03 08:29:12 +0100

    simpler code for UAV aging checks

--- src/features/uavs/saga.js
+++ src/features/uavs/saga.js
@@ -1,8 +1,7 @@
 import isEmpty from 'lodash-es/isEmpty';
 import mapValues from 'lodash-es/mapValues';
 import { eventChannel } from 'redux-saga';
-import { call, put, take } from 'redux-saga/effects';
-import TimerSet from 'tick-tock';
+import { all, call, delay, put, select, take } from 'redux-saga/effects';
 
 import { addUAVs, removeUAVs, updateAgesOfUAVs, updateUAVs } from './slice';
 
@@ -21,6 +20,47 @@ const convertUAVToRedux = uav => uav.toJSON();
 const convertUAVsToRedux = objects => mapValues(objects, convertUAVToRedux);
 
 /**
+ * Returns a proposed age code for a UAV, given the time when it was last
+ * updated and the current timestamp.
+ *
+ * @param  {number}  lastUpdatedAt  the time when the UAV was updated the
+ *         last time
+ * @param  {number}  now  the current timestamp, in milliseconds
+ * @return {string} the proposed new age code of the UAV
+ */
+function proposeAgeCode(lastUpdatedAt, now) {
+  const age = lastUpdatedAt ? now - lastUpdatedAt : 600000000;
+
+  if (age >= 600000) {
+    /* UAV was not seen for at least 10 minutes; remove it completely */
+    /* TODO(ntamas) */
+    return UAVAge.GONE;
+  }
+
+  if (age >= 60000) {
+    /* UAV was not seen for at least a minute */
+    return UAVAge.GONE;
+  }
+
+  if (age >= 3000) {
+    return UAVAge.INACTIVE;
+  }
+
+  return UAVAge.ACTIVE;
+}
+
+/**
+ * Schedules all the given UAVs to be checked whether we have seen a status
+ * message from them recently enough.
+ */
+function updateAgeCodeForUAVs(uavs) {
+  const now = Date.now();
+  for (const uav of uavs) {
+    uav.age = proposeAgeCode(uav.lastUpdatedAt, now);
+  }
+}
+
+/**
  * Subscribes to events originating from the given flock, putting appropriate
  * messages on a channel that leads back to the flock-to-Redux synchromizer
  * saga.
@@ -30,102 +70,28 @@ const convertUAVsToRedux = objects => mapValues(objects, convertUAVToRedux);
  */
 function subscribeToFlock(flock) {
   return eventChannel(emit => {
-    const updateTimer = Symbol('update');
-    const timers = new TimerSet();
-    const lastUpdated = {};
+    let updateTimer = null;
     let pendingUpdates = {};
-    let pendingAgeOnlyUpdates = {};
-
-    /**
-     * Checks whether we have seen a status message from the UAV with the given
-     * ID recently enough.
-     *
-     * @param  {string} uavId  the ID of the UAV to check
-     * @param  {float}  now    the current timestamp, in milliseconds
-     */
-    function checkUAVAgingById(uavId) {
-      let timeUntilNextCheck = -1;
-
-      const age =
-        uavId && lastUpdated[uavId]
-          ? Date.now() - lastUpdated[uavId]
-          : 600000000;
-
-      if (age >= 600000) {
-        /* UAV was not seen for at least 10 minutes; remove it completely */
-        /* TODO(ntamas) */
-        console.warn('UAV', uavId, 'should be removed');
-        timeUntilNextCheck = -1;
-      } else if (age >= 60000) {
-        /* UAV was not seen for at least a minute -- it has to be marked as
-         * gone and then checked again in 9 minutes */
-        onUAVAgeUpdated(uavId, UAVAge.GONE);
-        timeUntilNextCheck = 9 * 60000;
-      } else if (age >= 3000) {
-        /* UAV was not seen for at least 3 seconds -- it has to be marked as
-         * inactive and then checked again in 57 seconds */
-        onUAVAgeUpdated(uavId, UAVAge.INACTIVE);
-        timeUntilNextCheck = 57000;
-      } else {
-        /* UAV was seen recently -- check again at the time when it would
-         * become inactive */
-        timeUntilNextCheck = 3000 - age;
-      }
-
-      if (timeUntilNextCheck >= 0) {
-        if (timers.active(uavId)) {
-          timers.clear(uavId);
-        }
-
-        timers.setTimeout(
-          uavId,
-          () => checkUAVAgingById(uavId),
-          timeUntilNextCheck
-        );
-      }
-    }
-
-    /**
-     * Schedules all the given UAVs to be checked whether we have seen a status
-     * message from them recently enough.
-     */
-    function scheduleUAVAging(uavs) {
-      for (const uav of uavs) {
-        checkUAVAgingById(uav.id);
-      }
-    }
-
-    /**
-     * Unschedules all the given UAVs so we don't check them for recent status
-     * messages any more.
-     */
-    function unscheduleUAVAging(uavs) {
-      timers.clear(...uavs.map(uav => uav.id));
-    }
 
     /**
      * Flushes all pending updates of the UAVs to the Redux state store.
      */
     function flushUpdates() {
-      // There is at least one update, so send an event, and clear the updates.
+      // If there was at least one update, send an event and then schedule the
+      // next call.
+      if (!isEmpty(pendingUpdates)) {
+        emit(['updated', pendingUpdates]);
+        updateTimer = setTimeout(flushUpdates, 500);
+      } else {
+        updateTimer = null;
+      }
+
+      // Clear the updates.
       //
       // Note that we don't use the delete operator to clear the updates. This
       // is intentional; deleting properties would convert the objects from
       // fast to slow mode in the V8 JS engine.
-      emit(['updated', pendingUpdates]);
-
-      if (!isEmpty(pendingAgeOnlyUpdates)) {
-        emit(['aged', pendingAgeOnlyUpdates]);
-      }
-
-      // Kickstart the aging check again for the updated UAVs -- this is needed
-      // to resume checking when a UAV returns from the "gone" state
-      for (const uav of Object.values(pendingUpdates)) {
-        checkUAVAgingById(uav.id);
-      }
-
       pendingUpdates = {};
-      pendingAgeOnlyUpdates = {};
     }
 
     /**
@@ -133,23 +99,12 @@ function subscribeToFlock(flock) {
      * immediately and then schedules a new one. Otherwise does nothing.
      */
     function scheduleFlushUpdates() {
-      if (!timers.active(updateTimer)) {
+      if (updateTimer === null) {
         flushUpdates();
-        timers.setTimeout(updateTimer, flushUpdates, 500);
+        updateTimer = setTimeout(flushUpdates, 500);
       } else {
         // There is a scheduled time for sending a new batch so do nothing
       }
-    }
-
-    function onUAVAgeUpdated(uavId, age) {
-      console.warn(`UAV ${uavId} is now ${age}`);
-      if (pendingUpdates[uavId]) {
-        pendingUpdates[uavId].age = age;
-      } else {
-        pendingAgeOnlyUpdates[uavId] = age;
-      }
-
-      scheduleFlushUpdates();
     }
 
     function onUAVsAdded(uavs) {
@@ -159,15 +114,13 @@ function subscribeToFlock(flock) {
         if (uav.id !== undefined) {
           uav.age = UAVAge.ACTIVE;
           additions[uav.id] = uav;
-          lastUpdated[uav.id] = uav.lastUpdated;
         }
       }
 
+      updateAgeCodeForUAVs(uavs);
+
       // Dispatch an "added" event immediately - we do not batch these
       emit(['added', additions]);
-
-      // Schedule the aging of the UAVs
-      scheduleUAVAging(uavs);
     }
 
     function onUAVsRemoved(uavs) {
@@ -178,24 +131,18 @@ function subscribeToFlock(flock) {
       for (const uav of uavs) {
         if (uav.id !== undefined) {
           removals[uav.id] = uav;
-          delete lastUpdated[uav.id];
           delete pendingUpdates[uav.id];
         }
       }
 
       // Dispatch a "removed" event immediately - we do not batch these
       emit(['removed', removals]);
-
-      // Unschedule the aging of the UAVs in the timer wheel
-      unscheduleUAVAging(uavs);
     }
 
     function onUAVsUpdated(uavs) {
       // Update events are batched
       for (const uav of uavs) {
-        lastUpdated[uav.id] = uav.lastUpdated;
         pendingUpdates[uav.id] = uav;
-        delete pendingAgeOnlyUpdates[uav.id];
       }
 
       scheduleFlushUpdates();
@@ -212,19 +159,21 @@ function subscribeToFlock(flock) {
       flock.uavsRemoved.detach(bindings.uavsRemoved);
       flock.uavsUpdated.detach(bindings.uavsUpdated);
 
-      timers.end();
+      if (updateTimer !== null) {
+        clearTimeout(updateTimer);
+      }
     };
   });
 }
 
 /**
- * Compound saga related to the management of the connection to the upstream
- * Skybrush server.
+ * Saga that updates the status of the UAVs in the Redux store based on the
+ * status updates received from the given flock object.
  *
  * @param {Flock} flock  the UAV flock whose members should be synchronized with
  *        the Redux store
  */
-export default function* uavSyncSaga(flock) {
+function* uavSyncSaga(flock) {
   const chan = yield call(subscribeToFlock, flock);
 
   while (true) {
@@ -233,10 +182,6 @@ export default function* uavSyncSaga(flock) {
     switch (command) {
       case 'added':
         yield put(addUAVs(convertUAVsToRedux(args[0])));
-        break;
-
-      case 'aged':
-        yield put(updateAgesOfUAVs(args[0]));
         break;
 
       case 'removed':
@@ -251,4 +196,44 @@ export default function* uavSyncSaga(flock) {
         console.warn(`Unknown command in UAV sync saga: ${command}`);
     }
   }
+}
+
+/**
+ * Saga that updates the age (status) of the UAVs based on the timestamp we
+ * have received an update from them the last time.
+ */
+function* uavAgingSaga() {
+  while (true) {
+    yield delay(1000);
+
+    const uavs = yield select(state => state.uavs.byId);
+    const uavIds = yield select(state => state.uavs.order);
+    const now = Date.now();
+    const updates = {};
+
+    for (const uavId of uavIds) {
+      const uav = uavs[uavId];
+      if (uav) {
+        const newAgeCode = proposeAgeCode(uav.lastUpdated, now);
+        if (newAgeCode !== uav.age) {
+          updates[uavId] = newAgeCode;
+        }
+      }
+    }
+
+    if (!isEmpty(updates)) {
+      yield put(updateAgesOfUAVs(updates));
+    }
+  }
+}
+
+/**
+ * Compound saga related to the management of the connection to the upstream
+ * Skybrush server.
+ *
+ * @param {Flock} flock  the UAV flock whose members should be synchronized with
+ *        the Redux store
+ */
+export default function* uavManagementSaga(flock) {
+  yield all([uavSyncSaga(flock), uavAgingSaga()]);
 }
