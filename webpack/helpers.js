a6c02497f5bd90388d2ac2191bbbb7b311448964
Tamas Nepusz <ntamas@gmail.com>
2020-05-05 17:02:14 +0200

    removed unsupported key from HTML

--- webpack/helpers.js
+++ webpack/helpers.js
@@ -5,7 +5,7 @@ const outputDir = path.resolve(projectRoot, 'build');
 const htmlMetaTags = {
   "charset": "utf-8",
   "description": "Skybrush Live: The Next-generation Drone Light Show Software Suite",
-  "viewport": "initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,target-densitydpi=device-dpi,user-scalable=no",
+  "viewport": "initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no",
 
   "Content-Security-Policy": "script-src 'self' https://dev.virtualearth.net; connect-src * ws: wss:;",
   "X-UA-Compatible": "IE=edge",
