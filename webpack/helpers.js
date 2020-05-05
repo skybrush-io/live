668b565a564fcc26aba832a75e5321d246daa134
Tamas Nepusz <ntamas@gmail.com>
2020-05-05 14:04:54 +0200

    try to prevent zooming

--- webpack/helpers.js
+++ webpack/helpers.js
@@ -5,9 +5,10 @@ const outputDir = path.resolve(projectRoot, 'build');
 const htmlMetaTags = {
   "charset": "utf-8",
   "description": "Skybrush Live: The Next-generation Drone Light Show Software Suite",
-  
+  "viewport": "initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,target-densitydpi=device-dpi,user-scalable=no",
+
   "Content-Security-Policy": "script-src 'self' https://dev.virtualearth.net; connect-src * ws: wss:;",
-  "X-UA-Compatible": "IE=edge"
+  "X-UA-Compatible": "IE=edge",
 };
 
 module.exports = {
