f632504239bd138e25915d53b90233e7ad48bb97
Tamas Nepusz <ntamas@gmail.com>
2020-09-11 23:14:34 +0200

    removed predicates from UAVs layer type

--- src/views/map/layers/uavs.jsx
+++ src/views/map/layers/uavs.jsx
@@ -3,138 +3,31 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-
 import ActiveUAVsLayerSource from '../sources/ActiveUAVsLayerSource';
 
-import { setLayerParameterById } from '../../../actions/layers';
-import flock from '../../../flock';
-import { getSelection } from '../../../selectors/selection';
-import { mapViewCoordinateFromLonLat } from '../../../utils/geography';
-import makeLogger from '~/utils/logging';
-
-import { showNotification } from '~/features/snackbar/slice';
-
-const colors = ['blue', 'green', 'orange', 'pink', 'purple', 'yellow'];
-
-const logger = makeLogger('UAVsLayer');
-
-const validatePredicates = (predicates, errorHandler) => {
-  const validPredicates = {};
-  const invalidPredicates = [];
-
-  for (const color in predicates) {
-    try {
-      /* eslint no-new-func: "off", no-unused-vars: "off" */
-      const predicateFunction = new Function(
-        'id',
-        `return ${predicates[color]}`
-      );
-      // Testing the function for errors with a sample string parameter.
-      const testResult = predicateFunction('TEST-01');
-      // If it didn't produce any errors then it is stored.
-      validPredicates[color] = predicates[color];
-    } catch (error) {
-      invalidPredicates.push(color);
-      errorHandler(`Invalid color predicate for ${color}: "${error}"`);
-    }
-  }
-
-  return { validPredicates, invalidPredicates };
-};
+import { setLayerParameterById } from '~/actions/layers';
+import flock from '~/flock';
+import { getSelection } from '~/selectors/selection';
+import { mapViewCoordinateFromLonLat } from '~/utils/geography';
 
 // === Settings for this particular layer type ===
 
-class UAVsLayerSettingsPresentation extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      colorPredicates: props.layer.parameters.colorPredicates,
-      invalidPredicates: [],
-    };
-
-    this._makeChangeHandler = this._makeChangeHandler.bind(this);
-    this._handleKeyPress = this._handleKeyPress.bind(this);
-    this._handleClick = this._handleClick.bind(this);
-  }
-
-  render() {
-    const colorInputs = colors.map((color) => (
-      <TextField
-        key={`${color}_predicate_textfield`}
-        fullWidth
-        name={`${color}_predicate_textfield`}
-        label={color}
-        value={this.state.colorPredicates[color] || ''}
-        error={this.state.invalidPredicates.includes(color)}
-        onChange={this._makeChangeHandler(color)}
-        onKeyPress={this._handleKeyPress}
-      />
-    ));
-    return (
-      <div>
-        <p key='header'>
-          Color predicates (e.g. <code>i\id.\includes(’1’)i</code>)
-        </p>
-        {colorInputs}
-        <p style={{ textAlign: 'center' }}>
-          <Button onClick={this._handleClick}>Apply changes</Button>
-        </p>
-      </div>
-    );
-  }
-
-  _makeChangeHandler(color) {
-    return (event) => {
-      const colorPredicates = {
-        ...this.state.colorPredicates,
-        [color]: event.target.value,
-      };
-      this.setState({ colorPredicates });
-    };
-  }
-
-  _handleKeyPress(e) {
-    if (e.key === 'Enter') {
-      this._handleClick();
-    }
-  }
-
-  _handleClick() {
-    const { validPredicates, invalidPredicates } = validatePredicates(
-      this.state.colorPredicates,
-      logger.error
-    );
-
-    this.props.setLayerParameter('colorPredicates', {
-      ...this.props.layer.parameters.colorPredicates,
-      ...validPredicates,
-    });
-
-    this.setState({ invalidPredicates });
-  }
-}
+const UAVsLayerSettingsPresentation = () => null;
 
 UAVsLayerSettingsPresentation.propTypes = {
   layer: PropTypes.object,
   layerId: PropTypes.string,
 
   setLayerParameter: PropTypes.func,
-  showMessage: PropTypes.func,
 };
 
 export const UAVsLayerSettings = connect(
   // mapStateToProps
-  (state, ownProps) => ({}),
+  () => ({}),
   // mapDispatchToProps
   (dispatch, ownProps) => ({
     setLayerParameter: (parameter, value) => {
       dispatch(setLayerParameterById(ownProps.layerId, parameter, value));
-    },
-    showMessage: (message) => {
-      dispatch(showNotification(message));
     },
   })
 )(UAVsLayerSettingsPresentation);
