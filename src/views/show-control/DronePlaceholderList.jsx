f501b37e05e3ae6c3f47911ac1fb14c77927eb60
Tamas Nepusz <ntamas@gmail.com>
2020-09-14 08:42:35 +0200

    DronePlaceholderList items are now always sorted

--- src/views/show-control/DronePlaceholderList.jsx
+++ src/views/show-control/DronePlaceholderList.jsx
@@ -1,3 +1,4 @@
+import { orderBy } from 'natural-orderby';
 import PropTypes from 'prop-types';
 import React from 'react';
 
@@ -20,61 +21,71 @@ const DronePlaceholderList = ({
   preferEmptyMessage,
   successMessage,
   title,
-}) => (
-  <Box mt={1}>
-    <Box display='flex' flexDirection='row' alignItems='center' minHeight={44}>
-      {title && (
-        <Box key='lead' minWidth={85}>
-          {title}
-        </Box>
-      )}
-      {items.slice(0, maxCount).map((item, index) => (
-        <Box key={item} ml={index > 0 || title ? 1 : 0}>
-          <DronePlaceholder
-            label={
-              typeof item === 'number' ? formatMissionId(item) : String(item)
-            }
-          />
-        </Box>
-      ))}
-      {items.length > maxCount ? (
-        <Box key='more' ml={1} color='text.secondary'>
-          <Typography variant='body2'>
-            + {items.length - maxCount} more
-          </Typography>
-        </Box>
-      ) : null}
-      {items.length === 0 ? (
-        successMessage && !preferEmptyMessage ? (
-          <>
-            <Box key='ok' ml={title ? 1 : 0}>
-              <DronePlaceholder label='OK' status='success' />
+}) => {
+  const formattedAndSortedIds = orderBy(
+    items
+      .slice(0, maxCount)
+      .map((item) =>
+        typeof item === 'number' ? formatMissionId(item) : String(item)
+      )
+  );
+  return (
+    <Box mt={1}>
+      <Box
+        display='flex'
+        flexDirection='row'
+        alignItems='center'
+        minHeight={44}
+      >
+        {title && (
+          <Box key='lead' minWidth={85}>
+            {title}
+          </Box>
+        )}
+        {formattedAndSortedIds.map((itemId, index) => (
+          <Box key={itemId} ml={index > 0 || title ? 1 : 0}>
+            <DronePlaceholder label={itemId} />
+          </Box>
+        ))}
+        {items.length > maxCount ? (
+          <Box key='more' ml={1} color='text.secondary'>
+            <Typography variant='body2'>
+              + {items.length - maxCount} more
+            </Typography>
+          </Box>
+        ) : null}
+        {items.length === 0 ? (
+          successMessage && !preferEmptyMessage ? (
+            <>
+              <Box key='ok' ml={title ? 1 : 0}>
+                <DronePlaceholder label='OK' status='success' />
+              </Box>
+              <Box key='successMessage' color='success.main' ml={1}>
+                <Typography variant='body2'>{successMessage}</Typography>
+              </Box>
+            </>
+          ) : (
+            <Box
+              key='emptyMessage'
+              color={title ? 'text.secondary' : null}
+              ml={title ? 1 : 0}
+            >
+              <Typography variant='body2'>{emptyMessage}</Typography>
             </Box>
-            <Box key='successMessage' color='success.main' ml={1}>
-              <Typography variant='body2'>{successMessage}</Typography>
+          )
+        ) : null}
+        {actions && (
+          <>
+            <Box key='padding' flex={1} />
+            <Box key='actions' ml={1}>
+              {actions}
             </Box>
           </>
-        ) : (
-          <Box
-            key='emptyMessage'
-            color={title ? 'text.secondary' : null}
-            ml={title ? 1 : 0}
-          >
-            <Typography variant='body2'>{emptyMessage}</Typography>
-          </Box>
-        )
-      ) : null}
-      {actions && (
-        <>
-          <Box key='padding' flex={1} />
-          <Box key='actions' ml={1}>
-            {actions}
-          </Box>
-        </>
-      )}
+        )}
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
 
 DronePlaceholderList.propTypes = {
   actions: PropTypes.oneOfType([
