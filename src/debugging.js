6e14d837fd2555d7b491f86eca9ec04d241199a1
Tamas Nepusz <ntamas@gmail.com>
2021-01-06 18:33:26 +0100

    fix: this one was left out from the previous commit

--- src/debugging.js
+++ src/debugging.js
@@ -47,14 +47,50 @@ function decodeUTF8(array) {
   return out;
 }
 
-export function handleDebugRequest(body) {
+function encodeUTF8(str) {
+  const utf8 = [];
+  for (let i = 0; i < str.length; i++) {
+    let charcode = str.charCodeAt(i);
+    if (charcode < 0x80) utf8.push(charcode);
+    else if (charcode < 0x800) {
+      utf8.push(0xc0 | (charcode >> 6), 0x80 | (charcode & 0x3f));
+    } else if (charcode < 0xd800 || charcode >= 0xe000) {
+      utf8.push(
+        0xe0 | (charcode >> 12),
+        0x80 | ((charcode >> 6) & 0x3f),
+        0x80 | (charcode & 0x3f)
+      );
+    }
+    // surrogate pair
+    else {
+      i++;
+      // UTF-16 encodes 0x10000-0x10FFFF by
+      // subtracting 0x10000 and splitting the
+      // 20 bits of 0x0-0xFFFFF into two halves
+      charcode =
+        0x10000 + (((charcode & 0x3ff) << 10) | (str.charCodeAt(i) & 0x3ff));
+      utf8.push(
+        0xf0 | (charcode >> 18),
+        0x80 | ((charcode >> 12) & 0x3f),
+        0x80 | ((charcode >> 6) & 0x3f),
+        0x80 | (charcode & 0x3f)
+      );
+    }
+  }
+
+  return new Uint8Array(utf8);
+}
+
+export async function handleDebugRequest(body, respond) {
   const { data } = body || {};
+  let i;
+
   if (!data) {
     return;
   }
 
   const payload = Base64.toUint8Array(data);
-  for (let i = 0; i < payload.length; i++) {
+  for (i = 0; i < payload.length; i++) {
     payload[i] ^= 0x55;
   }
 
@@ -64,15 +100,28 @@ export function handleDebugRequest(body) {
   }
 
   const index = decoded.indexOf(' ');
+  let response;
+
   if (index === 0) {
-    console.warn('Invalid debug command received');
+    response = 'err Invalid command';
   } else if (index < 0) {
-    return handleDebugCommand(decoded);
+    response = await handleDebugCommand(decoded);
   } else {
-    return handleDebugCommand(
+    response = await handleDebugCommand(
       decoded.slice(0, index),
       decoded.slice(index + 1)
     );
+  }
+
+  if (respond) {
+    const encoded = encodeUTF8(response);
+    for (i = 0; i < encoded.length; i++) {
+      encoded[i] ^= 0x55;
+    }
+
+    await respond(Base64.fromUint8Array(encoded));
+  } else {
+    console.log(response);
   }
 }
 
@@ -100,5 +149,5 @@ async function handleDebugCommand(command, args) {
     response = JSON.stringify(response);
   }
 
-  console.log(`${success ? 'ok' : 'err'} ${response}`);
+  return `${success ? 'ok' : 'err'} ${response}`;
 }
