82d6b28028b950381542c326a1ceee5d0dca5ba5
Tamas Nepusz <ntamas@gmail.com>
2021-03-11 14:10:57 +0100

    style: don't use a smaller font in markers

--- src/components/chat/Marker.jsx
+++ src/components/chat/Marker.jsx
@@ -11,7 +11,6 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    fontSize: 'smaller',
     margin: theme.spacing(0.5, 0),
 
     '& .date': {
