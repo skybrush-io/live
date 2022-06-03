93857c6e15eef95de7242f4dec42a17c81b0dfdc
Donkó István <istvan.donko@gmail.com>
2022-06-03 21:49:46 +0200

    chore: delete leftover message types
    Probably accidentally left these orphaned constants behind after this commit:
    https://github.com/skybrush-io/live/commit/e65cc1e72e8a407a2046de2db53f8f36c2673f7c#diff-b16c863648188c8362095883c6b992a9516272e3fbbfff6c3262db4b163fce43

--- src/actions/types.js
+++
@@ -1,7 +0,0 @@
-/**
- * @file Central repository of action type constants used in the app.
- */
-
-// Actions related to the "Messages" dialog
-export const CLOSE_MESSAGES_DIALOG = 'CLOSE_MESSAGES_DIALOG';
-export const SHOW_MESSAGES_DIALOG = 'SHOW_MESSAGES_DIALOG';
