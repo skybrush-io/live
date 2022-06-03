c0276b2434004dbd414bdc83bdab0c031a78fa76
Donkó István <istvan.donko@gmail.com>
2022-06-03 21:13:20 +0200

    refactor(redux): prompt dialog

--- src/actions/types.js
+++ src/actions/types.js
@@ -5,8 +5,3 @@
 // Actions related to the "Messages" dialog
 export const CLOSE_MESSAGES_DIALOG = 'CLOSE_MESSAGES_DIALOG';
 export const SHOW_MESSAGES_DIALOG = 'SHOW_MESSAGES_DIALOG';
-
-// Actions related to the global prompt dialog
-export const CANCEL_PROMPT_DIALOG = 'CANCEL_PROMPT_DIALOG';
-export const SHOW_PROMPT_DIALOG = 'SHOW_PROMPT_DIALOG';
-export const SUBMIT_PROMPT_DIALOG = 'SUBMIT_PROMPT_DIALOG';
--- src/reducers/dialogs/prompt.js
+++ src/reducers/dialogs/prompt.js
@@ -1,15 +1,11 @@
 /**
- * @file Reducer function for handling the part of the state object that
+ * @file Redux slice for handling the part of the state object that
  * stores the state of the global prompt dialog that we (re)use for
  * single-line inputs (instead of `window.prompt`).
  */
 
-import { handleActions } from 'redux-actions';
-import u from 'updeep';
+import { createSlice } from '@reduxjs/toolkit';
 
-/**
- * The default settings for the part of the state object being defined here.
- */
 const defaultState = {
   cancelButtonLabel: 'Cancel',
   dialogVisible: false,
@@ -21,35 +17,57 @@ const defaultState = {
 };
 
 /**
- * The reducer function that handles actions related to the prompt dialog.
+ * The reducer that handles actions related to the prompt dialog.
  */
-const reducer = handleActions(
-  {
-    SHOW_PROMPT_DIALOG: (state, action) =>
-      // Nothing is kept from the previous state; this is intentional
-      ({
-        ...defaultState,
-        ...action.payload,
-        dialogVisible: true,
+const { reducer, actions } = createSlice({
+  name: 'prompt',
+
+  /**
+   * The default settings for the part of the state object being defined here.
+   */
+  initialState: { ...defaultState },
+
+  reducers: {
+    /**
+     * Action that closes the prompt dialog without submitting anything.
+     *
+     * This is not exported because the thunk version should be used instead.
+     */
+    _cancelPromptDialog(state) {
+      state.dialogVisible = false;
+    },
+
+    /**
+     * Action that will show the prompt dialog.
+     * This is not exported because the thunk version should be used instead.
+     */
+    _showPromptDialog: {
+      prepare: (message, options) => ({
+        payload: {
+          ...(typeof options === 'string'
+            ? { initialValue: options }
+            : options),
+          message,
+        },
       }),
+      reducer: (state, action) =>
+        // Nothing is kept from the previous state; this is intentional
+        ({
+          ...defaultState,
+          ...action.payload,
+          dialogVisible: true,
+        }),
+    },
 
-    SUBMIT_PROMPT_DIALOG: (state) =>
-      u(
-        {
-          dialogVisible: false,
-        },
-        state
-      ),
-
-    CANCEL_PROMPT_DIALOG: (state) =>
-      u(
-        {
-          dialogVisible: false,
-        },
-        state
-      ),
+    /**
+     * Action that submits the prompt dialog with the given value.
+     *
+     * This is not exported because the thunk version should be used instead.
+     */
+    _submitPromptDialog(state) {
+      state.dialogVisible = false;
+    },
   },
-  defaultState
-);
+});
 
-export default reducer;
+export { reducer as default, actions };
