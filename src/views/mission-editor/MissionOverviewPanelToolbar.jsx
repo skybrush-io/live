1a419215c2366299852a89beb71a51ac9f6f4028
Tamas Nepusz <ntamas@gmail.com>
2022-09-16 01:33:06 +0200

    feat: mission items can now be uploaded to a single drone

--- src/views/mission-editor/MissionOverviewPanelToolbar.jsx
+++ src/views/mission-editor/MissionOverviewPanelToolbar.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import Paper from '@material-ui/core/Paper';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -10,23 +11,26 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import ArrowDown from '@material-ui/icons/ArrowDropDown';
 import ArrowUp from '@material-ui/icons/ArrowDropUp';
+import DeleteIcon from '@material-ui/icons/Delete';
 import TakeoffIcon from '@material-ui/icons/FlightTakeoff';
 import LandIcon from '@material-ui/icons/FlightLand';
 import HomeIcon from '@material-ui/icons/Home';
-import DeleteIcon from '@material-ui/icons/Delete';
+import PlayArrow from '@material-ui/icons/PlayArrow';
 
+import ToolbarDivider from '~/components/ToolbarDivider';
 import {
   addNewMissionItem,
   moveSelectedMissionItemsByDelta,
   removeSelectedMissionItems,
+  uploadMissionItemsToSelectedUAV,
 } from '~/features/mission/actions';
 import {
   canMoveSelectedMissionItemsDown,
   canMoveSelectedMissionItemsUp,
   getSelectedMissionItemIds,
 } from '~/features/mission/selectors';
+import { getSingleSelectedUAVId } from '~/features/uavs/selectors';
 import { MissionItemType } from '~/model/missions';
-import ToolbarDivider from '~/components/ToolbarDivider';
 
 const useStyles = makeStyles(
   (theme) => ({
@@ -43,12 +47,14 @@ const useStyles = makeStyles(
 const MissionOverviewPanelToolbar = ({
   canMoveDown,
   canMoveUp,
+  canUpload,
   onAddLand,
   onAddReturnToHome,
   onAddTakeoff,
   onMoveDown,
   onMoveUp,
   onRemoveSelectedMissionItems,
+  onUploadMissionItems,
   selectedIds,
 }) => {
   const classes = useStyles();
@@ -84,6 +90,16 @@ const MissionOverviewPanelToolbar = ({
         >
           <DeleteIcon fontSize='small' />
         </IconButton>
+        <ToolbarDivider orientation='vertical' />
+        <Button
+          color='primary'
+          disabled={!canUpload}
+          size='small'
+          endIcon={<PlayArrow />}
+          onClick={onUploadMissionItems}
+        >
+          Upload
+        </Button>
       </Toolbar>
     </Paper>
   );
@@ -92,12 +108,14 @@ const MissionOverviewPanelToolbar = ({
 MissionOverviewPanelToolbar.propTypes = {
   canMoveDown: PropTypes.bool,
   canMoveUp: PropTypes.bool,
+  canUpload: PropTypes.bool,
   onAddTakeoff: PropTypes.func,
   onAddLand: PropTypes.func,
   onAddReturnToHome: PropTypes.func,
   onMoveDown: PropTypes.func,
   onMoveUp: PropTypes.func,
   onRemoveSelectedMissionItems: PropTypes.func,
+  onUploadMissionItems: PropTypes.func,
   selectedIds: PropTypes.arrayOf(PropTypes.string),
 };
 
@@ -106,6 +124,7 @@ export default connect(
   (state) => ({
     canMoveDown: canMoveSelectedMissionItemsDown(state),
     canMoveUp: canMoveSelectedMissionItemsUp(state),
+    canUpload: getSingleSelectedUAVId(state) !== undefined,
     selectedIds: getSelectedMissionItemIds(state),
   }),
   // mapDispatchToProps
@@ -116,5 +135,6 @@ export default connect(
     onMoveDown: () => moveSelectedMissionItemsByDelta(1),
     onMoveUp: () => moveSelectedMissionItemsByDelta(-1),
     onRemoveSelectedMissionItems: removeSelectedMissionItems,
+    onUploadMissionItems: uploadMissionItemsToSelectedUAV,
   }
 )(MissionOverviewPanelToolbar);
