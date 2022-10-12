55612df324f37f8feb3d5c790814974bbfeda892
István Donkó <istvan.donko@gmail.com>
2022-10-12 21:07:34 +0200

    refactor: simplify and improve FileButton

--- src/components/FileButton.jsx
+++ src/components/FileButton.jsx
@@ -1,85 +1,72 @@
 import PropTypes from 'prop-types';
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 import Button from '@material-ui/core/Button';
 
 /**
  * Button that triggers a file upload dialog when clicked.
  */
-const FileButton = ({
-  accepts,
-  extensions,
-  id,
-  multiple,
-  onSelected,
-  onSelectionFailed,
-  ...rest
-}) => {
-  const inputRef = useRef();
-
-  // This state variable will be used to force-clear the file input when the
-  // user selects a file, then attempts to select it again later. If we did not
-  // do this, the second selection would not succeed because no change event
-  // would be fired.
-  const [generation, setGeneration] = useState(0);
-
-  const onHandleSelection = useCallback(
-    (item) => {
-      item =
-        item && item.files ? (multiple ? item.files : item.files[0]) : null;
-
-      if (!item) {
-        return;
-      }
-
-      if (!accepts || accepts(item)) {
-        if (onSelected) {
-          onSelected(item);
-        }
-      } else if (onSelectionFailed) {
-        onSelectionFailed(item);
-      }
-
-      setGeneration(generation + 1);
+const FileButton = React.forwardRef(
+  (
+    {
+      accepts,
+      children,
+      filter,
+      multiple,
+      onSelected,
+      onSelectionFailed,
+      ...rest
     },
-    [accepts, generation, multiple, onSelected, onSelectionFailed]
-  );
+    ref
+  ) => {
+    // This state variable will be used to force-clear the file input when the
+    // user selects a file, then attempts to select it again later. If we did not
+    // do this, the second selection would not succeed because no change event
+    // would be fired.
+    const [generation, setGeneration] = useState(0);
 
-  const handleClick = useCallback(() => {
-    if (inputRef.current) {
-      inputRef.current.click();
-    }
-  }, []);
+    const onHandleSelection = useCallback(
+      (item) => {
+        item =
+          item && item.files ? (multiple ? item.files : item.files[0]) : null;
 
-  const inputId = id ? `${id}-input` : undefined;
-
-  return (
-    <>
-      <input
-        key={generation}
-        ref={inputRef}
-        accept={
-          extensions
-            ? extensions.map((ext) => `.${ext.toLowerCase()}`)
-            : undefined
+        if (!item) {
+          return;
         }
-        type='file'
-        id={inputId}
-        multiple={multiple}
-        style={{ display: 'none' }}
-        onChange={(event) => onHandleSelection(event.target)}
-      />
-      <label id={id} htmlFor={inputId}>
-        <Button {...rest} onClick={handleClick} />
-      </label>
-    </>
-  );
-};
+
+        if (!accepts || accepts(item)) {
+          if (onSelected) {
+            onSelected(item);
+          }
+        } else if (onSelectionFailed) {
+          onSelectionFailed(item);
+        }
+
+        setGeneration(generation + 1);
+      },
+      [accepts, generation, multiple, onSelected, onSelectionFailed]
+    );
+
+    return (
+      <Button ref={ref} component='label' {...rest}>
+        <input
+          key={generation}
+          hidden
+          type='file'
+          accept={filter}
+          multiple={multiple}
+          onChange={(event) => onHandleSelection(event.target)}
+        />
+        {children}
+      </Button>
+    );
+  }
+);
 
 FileButton.propTypes = {
   accepts: PropTypes.func,
-  extensions: PropTypes.arrayOf(PropTypes.string),
-  id: PropTypes.string,
+  children: PropTypes.node,
+  filter: PropTypes.arrayOf(PropTypes.string),
   multiple: PropTypes.bool,
   onSelected: PropTypes.func,
   onSelectionFailed: PropTypes.func,
