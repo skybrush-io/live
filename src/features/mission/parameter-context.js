14ccfe81c093974baf284dd5ae04236356fcf577
Tamas Nepusz <ntamas@gmail.com>
2023-01-01 15:44:32 +0100

    feat: ui:contextHint is now handled

--- src/features/mission/parameter-context.js
+++ src/features/mission/parameter-context.js
@@ -11,6 +11,20 @@
  * the values of parameters from the UI context.
  */
 
+import {
+  getFeatureById,
+  getSingleSelectedFeatureId,
+} from '~/features/map-features/selectors';
+import {
+  getCurrentGPSPositionByUavId,
+  getSingleSelectedUAVId,
+} from '~/features/uavs/selectors';
+import { FeatureType } from '~/model/features';
+import {
+  toScaledJSONFromLonLat,
+  toScaledJSONFromObject,
+} from '~/utils/geography';
+
 export const ParameterUIContext = {
   SELECTED_UAV_COORDINATE: 'selectedUAVCoordinate',
   SELECTED_POLYGON_FEATURE: 'selectedPolygonFeature',
@@ -33,8 +47,89 @@ export const KNOWN_UI_CONTEXTS = Object.values(ParameterUIContext);
 export function getParametersFromContext(parameterNamesByContext, getState) {
   const result = {};
 
-  // TODO(ntamas)
+  for (const [contextId, parameterNames] of parameterNamesByContext.entries()) {
+    const handler = contextHandlers[contextId];
+
+    if (handler && typeof handler === 'function') {
+      handler(result, parameterNames, getState);
+    }
+  }
+
   console.log(result);
 
   return result;
 }
+
+/**
+ * Helper function that assigns the same value to multiple keys in the given
+ * object.
+ */
+function assign(result, keys, value) {
+  if (typeof keys === 'string') {
+    keys = [keys];
+  }
+
+  for (const key of keys) {
+    result[key] = value;
+  }
+}
+
+/**
+ * Extracts the selected UAV from the store and assigns its coordinates to the
+ * listed variables in the result object.
+ */
+function extractSelectedUAVCoordinateFromContext(
+  result,
+  parameterNames,
+  getState
+) {
+  const state = getState();
+
+  const uavId = getSingleSelectedUAVId(state);
+  if (!uavId) {
+    throw new Error('Exactly one UAV must be selected');
+  }
+
+  const uavPosition = getCurrentGPSPositionByUavId(state, uavId);
+  if (!uavPosition) {
+    throw new Error('The selected UAV does not have a GPS position yet');
+  }
+
+  assign(result, parameterNames, toScaledJSONFromObject(uavPosition));
+}
+
+/**
+ * Extracts the selected polygon from the store and assigns its coordinates
+ * (both the boundary and any potential holes in it) to the listed variables in
+ * the result object.
+ */
+function extractSelectedPolygonFromContext(result, parameterNames, getState) {
+  const state = getState();
+
+  const featureId = getSingleSelectedFeatureId(state);
+  if (!featureId) {
+    throw new Error('Exactly one polygon must be selected on the map');
+  }
+
+  const feature = getFeatureById(state, featureId);
+  const featureType = feature?.type;
+  if (featureType !== FeatureType.POLYGON) {
+    throw new Error(
+      `The selected feature on the map must be a polygon, got: ${feature?.type}`
+    );
+  }
+
+  // Extend the parameters with the coordinates of the selected line string
+  // and the coordinates of the selected UAV
+  assign(result, parameterNames, {
+    points: feature.points.map(toScaledJSONFromLonLat),
+    holes: feature.holes.map((hole) => hole.map(toScaledJSONFromLonLat)),
+  });
+}
+
+const contextHandlers = {
+  [ParameterUIContext.SELECTED_UAV_COORDINATE]:
+    extractSelectedUAVCoordinateFromContext,
+  [ParameterUIContext.SELECTED_POLYGON_FEATURE]:
+    extractSelectedPolygonFromContext,
+};
