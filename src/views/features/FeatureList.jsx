18ef10aa8b71967f66bb547688568447574916c0
István Donkó <istvan.donko@gmail.com>
2023-01-05 14:05:36 +0100

    feat: add focus and visibility to the feature list

--- src/views/features/FeatureList.jsx
+++ src/views/features/FeatureList.jsx
@@ -7,7 +7,11 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+
 import Edit from '@material-ui/icons/Edit';
+import FilterCenterFocus from '@material-ui/icons/FilterCenterFocus';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
 import Tooltip from '@skybrush/mui-components/lib/Tooltip';
 
@@ -24,11 +28,14 @@ import {
 } from '~/features/map/selection';
 import { showFeatureEditorDialog } from '~/features/map-features/actions';
 import {
+  getFeaturesByIds,
   getFeaturesInOrder,
   getSelectedFeatureIds,
 } from '~/features/map-features/selectors';
+import { updateFeatureVisibility } from '~/features/map-features/slice';
 import { getNameOfFeatureType, getIconOfFeatureType } from '~/model/features';
 import { featureIdToGlobalId } from '~/model/identifiers';
+import { fitCoordinatesIntoMapView } from '~/signals';
 
 /**
  * Presentation component for a single entry in the feature list.
@@ -37,19 +44,44 @@ import { featureIdToGlobalId } from '~/model/identifiers';
  * @return {Object} the React presentation component
  */
 const FeatureListEntry = (props) => {
-  const { feature, onEditFeature, onSelectFeature, selected } = props;
-  const { id, color, label, type } = feature;
+  const {
+    feature,
+    onEditFeature,
+    onFocusFeature,
+    onSelectFeature,
+    onToggleFeatureVisibility,
+    selected,
+  } = props;
+  const { id, color, label, type, visible } = feature;
 
-  const actionButton = (
-    <Tooltip content='Feature properties'>
-      <IconButton edge='end' data-id={id} onClick={onEditFeature}>
-        <Edit />
-      </IconButton>
-    </Tooltip>
+  const actionButtons = (
+    <>
+      <Tooltip content='Focus feature'>
+        <IconButton edge='end' data-id={id} onClick={onFocusFeature}>
+          <FilterCenterFocus />
+        </IconButton>
+      </Tooltip>
+      <Tooltip content='Toggle visibility'>
+        <IconButton edge='end' data-id={id} onClick={onToggleFeatureVisibility}>
+          {visible ? <Visibility /> : <VisibilityOff color='disabled' />}
+        </IconButton>
+      </Tooltip>
+      <Tooltip content='Feature properties'>
+        <IconButton edge='end' data-id={id} onClick={onEditFeature}>
+          <Edit />
+        </IconButton>
+      </Tooltip>
+    </>
   );
 
   return (
-    <ListItem button selected={selected} data-id={id} onClick={onSelectFeature}>
+    <ListItem
+      button
+      style={{ paddingRight: '120px' }}
+      selected={selected}
+      data-id={id}
+      onClick={onSelectFeature}
+    >
       <ListItemIcon style={{ color }}>
         {getIconOfFeatureType(type)}
       </ListItemIcon>
@@ -58,14 +90,16 @@ const FeatureListEntry = (props) => {
       ) : (
         <ListItemText secondary={getNameOfFeatureType(type)} />
       )}
-      <ListItemSecondaryAction>{actionButton}</ListItemSecondaryAction>
+      <ListItemSecondaryAction>{actionButtons}</ListItemSecondaryAction>
     </ListItem>
   );
 };
 
 FeatureListEntry.propTypes = {
   onEditFeature: PropTypes.func,
+  onFocusFeature: PropTypes.func,
   onSelectFeature: PropTypes.func,
+  onToggleFeatureVisibility: PropTypes.func,
   feature: PropTypes.object.isRequired,
   selected: PropTypes.bool,
 };
@@ -74,14 +108,25 @@ FeatureListEntry.propTypes = {
  * Presentation component for the entire feature list.
  */
 export const FeatureListPresentation = listOf(
-  (feature, { onEditFeature, onSelectFeature, selectedFeatureIds }) => {
+  (
+    feature,
+    {
+      onEditFeature,
+      onFocusFeature,
+      onSelectFeature,
+      onToggleFeatureVisibility,
+      selectedFeatureIds,
+    }
+  ) => {
     return (
       <FeatureListEntry
         key={feature.id}
         feature={feature}
         selected={selectedFeatureIds.includes(feature.id)}
         onEditFeature={onEditFeature}
+        onFocusFeature={onFocusFeature}
         onSelectFeature={onSelectFeature}
+        onToggleFeatureVisibility={onToggleFeatureVisibility}
       />
     );
   },
@@ -97,6 +142,7 @@ export default connect(
   (state) => ({
     dense: true,
     features: getFeaturesInOrder(state),
+    featuresByIds: getFeaturesByIds(state),
     selectedFeatureIds: getSelectedFeatureIds(state),
   }),
   // mapDispatchToProps
@@ -118,6 +164,34 @@ export default connect(
 
       if (featureId) {
         dispatch(action([featureIdToGlobalId(featureId)]));
+      }
+    },
+    onSetFeatureVisibility(event, visible) {
+      const featureId = event.currentTarget.dataset.id;
+      if (featureId) {
+        dispatch(updateFeatureVisibility({ id: featureId, visible }));
+      }
+    },
+  }),
+  // mergeProps
+  (
+    { featuresByIds, ...stateProps },
+    { onSetFeatureVisibility, ...dispatchProps },
+    ownProps
+  ) => ({
+    ...ownProps,
+    ...stateProps,
+    ...dispatchProps,
+    onFocusFeature(event) {
+      const featureId = event.currentTarget.dataset.id;
+      if (featureId) {
+        fitCoordinatesIntoMapView(featuresByIds[featureId].points);
+      }
+    },
+    onToggleFeatureVisibility(event) {
+      const featureId = event.currentTarget.dataset.id;
+      if (featureId) {
+        onSetFeatureVisibility(event, !featuresByIds[featureId].visible);
       }
     },
   })
