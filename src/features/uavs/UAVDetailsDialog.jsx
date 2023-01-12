cf47c0376d537c0d20f4fb6b9e059017bf9cb503
István Donkó <istvan.donko@gmail.com>
2023-01-12 03:17:32 +0100

    feat: make the UAV details dialog resizable

--- src/features/uavs/UAVDetailsDialog.jsx
+++ src/features/uavs/UAVDetailsDialog.jsx
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
+import { ResizableBox } from 'react-resizable';
 
 import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
 
 import DraggableDialog from '@skybrush/mui-components/lib/DraggableDialog';
 
@@ -15,24 +17,62 @@ import UAVDetailsDialogBody from './UAVDetailsDialogBody';
 import UAVDetailsDialogSidebar from './UAVDetailsDialogSidebar';
 import UAVDetailsDialogTabs from './UAVDetailsDialogTabs';
 
+const useStyles = makeStyles(
+  (theme) => ({
+    resizeHandle: {
+      width: 15,
+      height: 50,
+
+      position: 'absolute',
+      right: 0,
+      top: '50%',
+
+      cursor: 'ew-resize',
+
+      '&:after': {
+        content: '""',
+        display: 'block',
+
+        width: 10,
+        height: 50,
+
+        borderRight: `5px dotted ${theme.palette.action.selected}`,
+      },
+    },
+  }),
+  { name: 'UAVDetailsDialog' }
+);
+
+const ResizeHandle = React.forwardRef((props, ref) => {
+  const classes = useStyles();
+  return <Box ref={ref} className={classes.resizeHandle} {...props} />;
+});
+
 /**
  * Presentation component for the dialog that allows the user to inspect the
  * details of a specific UAV.
  */
 const UAVDetailsDialog = ({ onClose, open }) => (
   <DraggableDialog
-    fullWidth
     open={open}
-    maxWidth='sm'
+    maxWidth={false}
     sidebarComponents={<UAVDetailsDialogSidebar />}
     toolbarComponent={(dragHandleId) => (
       <UAVDetailsDialogTabs dragHandle={dragHandleId} />
     )}
     onClose={onClose}
   >
-    <Box position='relative' height={448} overflow='auto'>
-      <UAVDetailsDialogBody />
-    </Box>
+    <ResizableBox
+      width={414}
+      height={448}
+      minConstraints={[414, 448]}
+      axis='x'
+      handle={<ResizeHandle />}
+    >
+      <Box position='relative' height={448} overflow='auto'>
+        <UAVDetailsDialogBody />
+      </Box>
+    </ResizableBox>
   </DraggableDialog>
 );
 
