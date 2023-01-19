4e0609ced656bc4c5f1b8ac8deb4d41157c4c472
István Donkó <istvan.donko@gmail.com>
2023-01-19 23:03:32 +0100

    feat: store the position of the UAV details dialog

--- src/features/uavs/UAVDetailsDialog.jsx
+++ src/features/uavs/UAVDetailsDialog.jsx
@@ -1,3 +1,4 @@
+import clamp from 'lodash-es/clamp';
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
@@ -9,19 +10,24 @@ import { makeStyles } from '@material-ui/core/styles';
 import DraggableDialog from '@skybrush/mui-components/lib/DraggableDialog';
 
 import {
+  UAV_DETAILS_DIALOG_BODY_HEIGHT as BODY_HEIGHT,
+  UAV_DETAILS_DIALOG_BODY_MIN_WIDTH as BODY_MIN_WIDTH,
+  UAV_DETAILS_DIALOG_HEIGHT as HEIGHT,
+  UAV_DETAILS_DIALOG_SIDEBAR_WIDTH as SIDEBAR_WIDTH,
+} from './constants';
+
+import {
   closeUAVDetailsDialog,
+  getUAVDetailsDialogPosition,
   getUAVDetailsDialogWidth,
   isUAVDetailsDialogOpen,
+  setUAVDetailsDialogPosition,
   setUAVDetailsDialogWidth,
 } from './details';
 
 import UAVDetailsDialogBody from './UAVDetailsDialogBody';
 import UAVDetailsDialogSidebar from './UAVDetailsDialogSidebar';
 import UAVDetailsDialogTabs from './UAVDetailsDialogTabs';
-import {
-  UAV_DETAILS_DIALOG_MIN_WIDTH,
-  UAV_DETAILS_DIALOG_HEIGHT,
-} from './constants';
 
 const useStyles = makeStyles(
   (theme) => ({
@@ -63,8 +69,23 @@ ResizeHandle.propTypes = {
  * Presentation component for the dialog that allows the user to inspect the
  * details of a specific UAV.
  */
-const UAVDetailsDialog = ({ initialWidth, onClose, onResizeStop, open }) => {
-  const [width, setWidth] = useState(initialWidth);
+const UAVDetailsDialog = ({
+  initialPosition,
+  initialWidth,
+  onClose,
+  onDragStop,
+  onResizeStop,
+  open,
+}) => {
+  const horizontalBound = (window.innerWidth - initialWidth) / 2;
+  const verticalBound = (window.innerHeight - HEIGHT) / 2;
+
+  const defaultPosition = {
+    x: clamp(initialPosition.x, -horizontalBound, horizontalBound),
+    y: clamp(initialPosition.y, -verticalBound, verticalBound),
+  };
+
+  const [width, setWidth] = useState(initialWidth - SIDEBAR_WIDTH);
 
   const onResize = (_event, { size }) => {
     setWidth(size.width);
@@ -72,6 +93,7 @@ const UAVDetailsDialog = ({ initialWidth, onClose, onResizeStop, open }) => {
 
   return (
     <DraggableDialog
+      DraggableProps={{ bounds: 'parent', defaultPosition, onStop: onDragStop }}
       open={open}
       maxWidth={false}
       sidebarComponents={<UAVDetailsDialogSidebar />}
@@ -82,11 +104,8 @@ const UAVDetailsDialog = ({ initialWidth, onClose, onResizeStop, open }) => {
     >
       <Resizable
         width={width}
-        height={UAV_DETAILS_DIALOG_HEIGHT}
-        minConstraints={[
-          UAV_DETAILS_DIALOG_MIN_WIDTH,
-          UAV_DETAILS_DIALOG_HEIGHT,
-        ]}
+        height={BODY_HEIGHT}
+        minConstraints={[BODY_MIN_WIDTH, BODY_HEIGHT]}
         axis='x'
         handle={<ResizeHandle />}
         onResize={onResize}
@@ -97,7 +116,7 @@ const UAVDetailsDialog = ({ initialWidth, onClose, onResizeStop, open }) => {
           position='relative'
           width={width}
           maxWidth='100%'
-          height={UAV_DETAILS_DIALOG_HEIGHT}
+          height={BODY_HEIGHT}
           overflow='auto'
         >
           <UAVDetailsDialogBody />
@@ -108,8 +127,13 @@ const UAVDetailsDialog = ({ initialWidth, onClose, onResizeStop, open }) => {
 };
 
 UAVDetailsDialog.propTypes = {
+  initialPosition: PropTypes.shape({
+    x: PropTypes.number,
+    y: PropTypes.number,
+  }),
   initialWidth: PropTypes.number,
   onClose: PropTypes.func,
+  onDragStop: PropTypes.func,
   onResizeStop: PropTypes.func,
   open: PropTypes.bool,
 };
@@ -117,6 +141,7 @@ UAVDetailsDialog.propTypes = {
 export default connect(
   // mapStateToProps
   (state) => ({
+    initialPosition: getUAVDetailsDialogPosition(state),
     initialWidth: getUAVDetailsDialogWidth(state),
     open: isUAVDetailsDialogOpen(state),
   }),
@@ -124,10 +149,15 @@ export default connect(
   // mapDispatchToProps
   {
     onClose: closeUAVDetailsDialog,
+    onDragStop:
+      (_event, { x, y }) =>
+      (dispatch) => {
+        dispatch(setUAVDetailsDialogPosition({ x, y }));
+      },
     onResizeStop:
       (_event, { size }) =>
       (dispatch) => {
-        dispatch(setUAVDetailsDialogWidth(size.width));
+        dispatch(setUAVDetailsDialogWidth(size.width + SIDEBAR_WIDTH));
       },
   }
 )(UAVDetailsDialog);
