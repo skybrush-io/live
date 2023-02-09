d36a8bf3c4d03d28065ebc1b2852cd9e0489e251
István Donkó <istvan.donko@gmail.com>
2023-02-09 19:37:08 +0100

    feat: make the splash screen icon configurable

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -1,15 +1,15 @@
+import config from 'config';
+
 import PropTypes from 'prop-types';
 import React from 'react';
 import { CoverPagePresentation as CoverPage } from 'react-cover-page';
 
 import 'react-cover-page/themes/default.css';
 
-import logo from '~/../assets/icons/splash.png';
-
 const SplashScreen = ({ loading, visible }) => (
   <CoverPage
     loading={loading}
-    icon={<img src={logo} width={96} height={96} alt='' />}
+    icon={<img src={config.branding.splashIcon} width={96} height={96} />}
     title={
       <span>
         skybrush <b style={{ fontWeight: 400 }}>live</b>
