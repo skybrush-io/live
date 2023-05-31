b68e4095603b4ad0f0bfed1ecd9f8d57aff98c83
István Donkó <istvan.donko@gmail.com>
2023-06-01 00:03:33 +0200

    feat: globalize the UAV dialog opening hotkey

--- src/features/hotkeys/signal.js
+++ src/features/hotkeys/signal.js
@@ -2,6 +2,11 @@ import Signal from 'mini-signals';
 
 const hotkeySignal = new Signal();
 
+export const isKeyboardNavigationActive = () =>
+  // TODO: Update to `mini-signals@2.0.0`, which explicitly has such a check:
+  // https://github.com/Hypercubed/mini-signals/blob/v2.0.0/docs/classes/MiniSignal.md#haslisteners
+  hotkeySignal.handlers().length > 0;
+
 export const sendKeyboardNavigationSignal = (action) => (event) =>
   hotkeySignal.dispatch(action, event);
 
