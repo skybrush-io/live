3bb508297b2ed539c75f1b2143dbb2bc71bd6c7e
István Donkó <istvan.donko@gmail.com>
2023-06-20 20:33:39 +0200

    refactor: annotate settings enums with types

--- src/model/settings.ts
+++ src/model/settings.ts
@@ -2,42 +2,45 @@
  * @file Model objects and classes related to the settings of the app.
  */
 
-export const AltitudeSummaryType = {
-  AMSL: 'amsl',
-  AHL: 'ahl',
-  AGL: 'agl',
-  XYZ: 'xyz',
-};
+export enum AltitudeSummaryType {
+  AMSL = 'amsl',
+  AHL = 'ahl',
+  AGL = 'agl',
+  XYZ = 'xyz',
+}
 
-const _altitudeSummaryTypeDescriptions = {
+const altitudeSummaryTypeDescriptions = {
   [AltitudeSummaryType.AMSL]: 'Altitude above mean sea level',
   [AltitudeSummaryType.AHL]: 'Altitude above home level',
   [AltitudeSummaryType.AGL]: 'Altitude above ground level',
   [AltitudeSummaryType.XYZ]: 'Altitude in local coordinate system',
 };
 
-const _altitudeSummaryTypeShortDescriptions = {
+const altitudeSummaryTypeShortDescriptions = {
   [AltitudeSummaryType.AMSL]: 'AMSL',
   [AltitudeSummaryType.AHL]: 'AHL',
   [AltitudeSummaryType.AGL]: 'AGL',
   [AltitudeSummaryType.XYZ]: 'Z coordinate',
 };
 
-export const describeAltitudeSummaryType = (type, { short } = {}) =>
+export const describeAltitudeSummaryType = (
+  type: AltitudeSummaryType,
+  { short = false } = {}
+): string =>
   (short
-    ? _altitudeSummaryTypeShortDescriptions
-    : _altitudeSummaryTypeDescriptions)[type] || 'Unknown altitude type';
+    ? altitudeSummaryTypeShortDescriptions
+    : altitudeSummaryTypeDescriptions)[type] || 'Unknown altitude type';
 
-export const CoordinateFormat = {
-  DEGREES: 'd',
-  DEGREES_MINUTES: 'dm',
-  DEGREES_MINUTES_SECONDS: 'dms',
-  SIGNED_DEGREES: '-d',
-  SIGNED_DEGREES_MINUTES: '-dm',
-  SIGNED_DEGREES_MINUTES_SECONDS: '-dms',
-};
+export enum CoordinateFormat {
+  DEGREES = 'd',
+  DEGREES_MINUTES = 'dm',
+  DEGREES_MINUTES_SECONDS = 'dms',
+  SIGNED_DEGREES = '-d',
+  SIGNED_DEGREES_MINUTES = '-dm',
+  SIGNED_DEGREES_MINUTES_SECONDS = '-dms',
+}
 
-const _coordinateFormatDescriptions = {
+const coordinateFormatDescriptions = {
   [CoordinateFormat.DEGREES]: 'Decimal degrees',
   [CoordinateFormat.DEGREES_MINUTES]: 'Degrees and minutes',
   [CoordinateFormat.DEGREES_MINUTES_SECONDS]: 'Degrees, minutes and seconds',
@@ -47,16 +50,16 @@ const _coordinateFormatDescriptions = {
     'Degrees, minutes and seconds (signed)',
 };
 
-export const describeCoordinateFormat = (format) =>
-  _coordinateFormatDescriptions[format] || 'Unknown format';
+export const describeCoordinateFormat = (format: CoordinateFormat): string =>
+  coordinateFormatDescriptions[format] || 'Unknown format';
 
-export const BatteryDisplayStyle = {
-  VOLTAGE: 'voltage',
-  PERCENTAGE: 'percentage',
-  FORCED_PERCENTAGE: 'forcedPercentage',
-};
+export enum BatteryDisplayStyle {
+  VOLTAGE = 'voltage',
+  PERCENTAGE = 'percentage',
+  FORCED_PERCENTAGE = 'forcedPercentage',
+}
 
-const _batteryDisplayStyleDescriptions = {
+const batteryDisplayStyleDescriptions = {
   [BatteryDisplayStyle.VOLTAGE]: 'Prefer voltage',
   [BatteryDisplayStyle.PERCENTAGE]:
     'Prefer percentage and show voltage if unknown',
@@ -64,5 +67,6 @@ const _batteryDisplayStyleDescriptions = {
     'Prefer percentage and estimate it from voltage if needed',
 };
 
-export const describeBatteryDisplayStyle = (style) =>
-  _batteryDisplayStyleDescriptions[style] || 'Unknown style';
+export const describeBatteryDisplayStyle = (
+  style: BatteryDisplayStyle
+): string => batteryDisplayStyleDescriptions[style] || 'Unknown style';
