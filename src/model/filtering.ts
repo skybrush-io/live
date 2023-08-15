b880ffef1a20aa62ede4c287ed57cd57e7b3761a
István Donkó <istvan.donko@gmail.com>
2023-08-15 16:30:21 +0200

    refactor: annotate the filtering model with types

--- src/model/filtering.ts
+++ src/model/filtering.ts
@@ -1,7 +1,6 @@
-import max from 'lodash-es/max';
-import memoize from 'memoizee';
+import * as memoize from 'memoizee';
 
-import { UAVAge } from './uav';
+import { type SerializedUAV, UAVAge } from './uav';
 import {
   isErrorCodeOrMoreSevere,
   isWarningCodeOrMoreSevere,
@@ -10,32 +9,32 @@ import {
 /**
  * Enum that describes the possible filtering presets for a list that shows UAVs.
  */
-export const UAVFilter = {
-  DEFAULT: 'default',
-  WITH_WARNINGS: 'withWarnings',
-  WITH_ERRORS: 'withErrors',
-  INACTIVE_ONLY: 'inactiveOnly',
+export enum UAVFilter {
+  DEFAULT = 'default',
+  WITH_WARNINGS = 'withWarnings',
+  WITH_ERRORS = 'withErrors',
+  INACTIVE_ONLY = 'inactiveOnly',
   // INACTIVE_ONLY could be NO_TELEMETRY after the rename, but we cannot change
   // its string value because people might have saved state slices with
   // 'inactiveOnly' and it would be a fuss to migrate these. So, to keep things
   // simple and consistent, we kept the old naming and changed the _displayed_
   // labels only.
-};
+}
 
 /**
  * Order in which the UAV filter presets should appear on the UI.
  */
-export const UAVFilters = [
+export const UAVFilters: readonly UAVFilter[] = [
   UAVFilter.DEFAULT,
   UAVFilter.WITH_WARNINGS,
   UAVFilter.WITH_ERRORS,
   UAVFilter.INACTIVE_ONLY,
-];
+] as const;
 
 /**
  * Human-readable labels that should be used on the UI to represent a UAV filter preset.
  */
-export const labelsForUAVFilter = {
+export const labelsForUAVFilter: Record<UAVFilter, string> = {
   [UAVFilter.DEFAULT]: 'All',
   [UAVFilter.WITH_WARNINGS]: 'Warnings and errors',
   [UAVFilter.WITH_ERRORS]: 'Errors only',
@@ -45,31 +44,35 @@ export const labelsForUAVFilter = {
 /**
  * Human-readable short labels that should be used on the UI to represent a UAV filter preset.
  */
-export const shortLabelsForUAVFilter = {
+export const shortLabelsForUAVFilter: Record<UAVFilter, string> = {
   [UAVFilter.DEFAULT]: 'All',
   [UAVFilter.WITH_WARNINGS]: 'Warn/Err',
   [UAVFilter.WITH_ERRORS]: 'Errors',
   [UAVFilter.INACTIVE_ONLY]: 'No telem',
 };
 
-export const getFilterFunctionForUAVFilter = memoize((filterId) => {
-  switch (filterId) {
-    case UAVFilter.WITH_ERRORS:
-      return (uav) =>
-        Array.isArray(uav?.errors) && uav.errors.length > 0
-          ? isErrorCodeOrMoreSevere(max(uav.errors))
-          : false;
+export const getFilterFunctionForUAVFilter = memoize(
+  (
+    filterId: UAVFilter
+  ): ((uav: SerializedUAV | undefined) => boolean) | undefined => {
+    switch (filterId) {
+      case UAVFilter.WITH_ERRORS:
+        return (uav) =>
+          uav && Array.isArray(uav.errors) && uav.errors.length > 0
+            ? isErrorCodeOrMoreSevere(Math.max(...uav.errors))
+            : false;
 
-    case UAVFilter.WITH_WARNINGS:
-      return (uav) =>
-        Array.isArray(uav?.errors) && uav.errors.length > 0
-          ? isWarningCodeOrMoreSevere(max(uav.errors))
-          : false;
+      case UAVFilter.WITH_WARNINGS:
+        return (uav) =>
+          uav && Array.isArray(uav.errors) && uav.errors.length > 0
+            ? isWarningCodeOrMoreSevere(Math.max(...uav.errors))
+            : false;
 
-    case UAVFilter.INACTIVE_ONLY:
-      return (uav) => uav?.age === UAVAge.INACTIVE;
+      case UAVFilter.INACTIVE_ONLY:
+        return (uav) => uav?.age === UAVAge.INACTIVE;
 
-    default:
-      return undefined;
+      default:
+        return undefined;
+    }
   }
-});
+);
