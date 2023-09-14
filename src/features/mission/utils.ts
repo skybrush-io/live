481d690b509a597be386134117ad654eb10e5184
István Donkó <istvan.donko@gmail.com>
2023-09-14 22:48:20 +0200

    refactor: annotate the mission slice with types

--- src/features/mission/utils.ts
+++ src/features/mission/utils.ts
@@ -1,5 +1,9 @@
 import isNil from 'lodash-es/isNil';
 
+import { type Nullable } from '~/utils/types';
+
+import { type MissionSliceState } from './slice';
+
 /**
  * Helper function that takes an array and returns an array that is a shallow
  * copy of the input array, but its length is constrained to a given value.
@@ -10,11 +14,14 @@ import isNil from 'lodash-es/isNil';
  * When the input array is longer than the desired length, it will be
  * truncated.
  */
-export function copyAndEnsureLengthEquals(length, input) {
+export function copyAndEnsureLengthEquals<T>(
+  length: number,
+  input: T[]
+): Array<Nullable<T>> {
   const inputLength = input.length;
 
   if (inputLength < length) {
-    const result = input.concat();
+    const result: Array<Nullable<T>> = input.concat();
     result.length = length;
     result.fill(null, inputLength);
     return result;
@@ -23,18 +30,29 @@ export function copyAndEnsureLengthEquals(length, input) {
   return input.slice(0, length);
 }
 
+export type MissionMappingEditorContinuation =
+  | 'next'
+  | 'prev'
+  | 'nextEmpty'
+  | 'prevEmpty';
+
 /**
  * Returns the index of the mapping slot to edit next after the termination
  * of an editing session.
  *
- * @param  {object} state        the mission-related slice of the state object
- * @param  {string} continuation string describing what to do next:
- *         'next' to move to the next slot, 'prev' to move to the previous
- *         slot, 'nextEmpty' to move to the next empty slot, 'prevEmpty' to
- *         move to the previous empty slot, wrapping around at the edges.
- * @return {number}              [description]
+ * @param state - The mission-related slice of the state object
+ * @param continuation - String describing what to do next:
+ *                       + 'next' to move to the next slot
+ *                       + 'prev' to move to the previous slot
+ *                       + 'nextEmpty' to move to the next empty slot
+ *                       + 'prevEmpty' to move to the previous empty slot
+ *                       (wrapping around at the edges)
+ * @returns The index of the next mission slot to edit
  */
-export function getNewEditIndex(state, continuation) {
+export function getNewEditIndex(
+  state: MissionSliceState,
+  continuation: MissionMappingEditorContinuation
+): number {
   const { mapping } = state;
   const currentIndex = state.mappingEditor.indexBeingEdited;
   const numberItems = mapping.length;
