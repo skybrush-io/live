751704ad5893ea0ee101d25327b97cdd7ef6d017
István Donkó <istvan.donko@gmail.com>
2024-01-17 21:40:06 +0100

    l10n: add German and Italian as available language

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,11 +1,15 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
+import de from './de';
 import en from './en';
+import it from './it';
 import hu from './hu';
 
 export const availableLanguages = [
+  { code: 'de', label: 'Deutsch' },
   { code: 'en', label: 'English' },
+  { code: 'it', label: 'Italiano' },
   { code: 'hu', label: 'Magyar' },
 ];
 
@@ -14,7 +18,9 @@ i18n.use(initReactI18next).init({
   fallbackLng: 'en',
   lng: 'en', // TODO: Make this an option in the config file
   resources: {
+    de: { translation: de },
     en: { translation: en },
+    it: { translation: it },
     hu: { translation: hu },
   },
 });
