c68101b14f05e4373f2f2cd4448ede2161460f0b
István Donkó <istvan.donko@gmail.com>
2024-01-18 12:25:35 +0100

    l10n: add Spanish as available language

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,26 +3,26 @@ import { initReactI18next } from 'react-i18next';
 
 import de from './de';
 import en from './en';
-import it from './it';
+import es from './es';
 import hu from './hu';
+import it from './it';
 
+// Labels (and sorting) based on: https://ux.stackexchange.com/q/37017/165102
 export const availableLanguages = [
-  { code: 'de', label: 'Deutsch' },
-  { code: 'en', label: 'English' },
-  { code: 'it', label: 'Italiano' },
-  { code: 'hu', label: 'Magyar' },
+  { label: 'Deutsch', code: 'de', translation: de },
+  { label: 'English', code: 'en', translation: en },
+  { label: 'Español', code: 'es', translation: es },
+  { label: 'Italiano', code: 'it', translation: it },
+  { label: 'Magyar', code: 'hu', translation: hu },
 ];
 
 const i18n = i18next.createInstance();
 i18n.use(initReactI18next).init({
   fallbackLng: 'en',
   lng: 'en', // TODO: Make this an option in the config file
-  resources: {
-    de: { translation: de },
-    en: { translation: en },
-    it: { translation: it },
-    hu: { translation: hu },
-  },
+  resources: Object.fromEntries(
+    availableLanguages.map(({ code, translation }) => [code, { translation }])
+  ),
 });
 
 export default i18n;
