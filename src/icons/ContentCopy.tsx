ecbdd6cb34c6e0d37dcc44ab183c62d9ae161302
István Donkó <istvan.donko@gmail.com>
2024-03-07 02:02:46 +0100

    refactor: make the creation of icons consistent

--- src/icons/ContentCopy.tsx
+++ src/icons/ContentCopy.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 import { createSvgIcon } from '@material-ui/core/utils';
 
 export default createSvgIcon(
-  <path d='M0 0h24v24H0z' fill='none' />,
-  'default ContentCopy'
+  <path d='M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2m0 16H8V7h11z' />,
+  'ContentCopy'
 );
