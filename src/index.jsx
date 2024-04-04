3804fa284939dc783868904f6b0457d89aa2c8e6
István Donkó <istvan.donko@gmail.com>
2024-04-04 20:57:44 +0200

    fix: switch font package and enable extra accents

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
+import '@fontsource/fira-sans/latin.css';
+import '@fontsource/fira-sans/latin-ext.css';
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import React from 'react';
 import { render } from 'react-dom';
-import 'typeface-fira-sans';
 
 import './i18n';
 import AppWithSplashScreen from './splash';
