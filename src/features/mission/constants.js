a5fd00940728c86b4a2d5f4b88adaf7ebb02ea62
István Donkó <istvan.donko@gmail.com>
2024-04-10 12:11:08 +0200

    feat: support uploading of multi-UAV mission items

--- src/features/mission/constants.js
+++ src/features/mission/constants.js
@@ -2,3 +2,11 @@
  * Type of the upload job corresponding to mission item uploads.
  */
 export const JOB_TYPE = 'missionItemUpload';
+
+/**
+ * Singleton job that represents the uploading of the current mission items to
+ * the drones.
+ */
+export const MISSION_ITEM_UPLOAD_JOB = Object.freeze({
+  type: JOB_TYPE,
+});
