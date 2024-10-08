34d4e9df48900064248ddd1b81742407fb872d54
Tamas Nepusz <ntamas@gmail.com>
2024-10-08 17:33:57 +0200

    chore: typing improvements

--- src/features/show/selectors.d.ts
+++ src/features/show/selectors.d.ts
@@ -1,3 +1,19 @@
 import type { RootState } from '~/store/reducers';
 
+export const areManualPreflightChecksSignedOff: (state: RootState) => boolean;
+export const areOnboardPreflightChecksSignedOff: (state: RootState) => boolean;
+export const areStartConditionsSyncedWithServer: (state: RootState) => boolean;
+export const didLastLoadingAttemptFail: (state: RootState) => boolean;
+export const didStartConditionSyncFail: (state: RootState) => boolean;
+export const hasLoadedShowFile: (state: RootState) => boolean;
+export const hasScheduledStartTime: (state: RootState) => boolean;
+export const hasShowChangedExternallySinceLoaded: (state: RootState) => boolean;
+export const hasShowOrigin: (state: RootState) => boolean;
 export const getShowStartTimeAsString: (state: RootState) => string;
+export const isLoadingShowFile: (state: RootState) => boolean;
+export const isShowAuthorizedToStart: (state: RootState) => boolean;
+export const isShowAuthorizedToStartLocally: (state: RootState) => boolean;
+export const isShowConvexHullInsideGeofence: (state: RootState) => boolean;
+export const isShowIndoor: (state: RootState) => boolean;
+export const isShowOutdoor: (state: RootState) => boolean;
+export const isTakeoffAreaApproved: (state: RootState) => boolean;
