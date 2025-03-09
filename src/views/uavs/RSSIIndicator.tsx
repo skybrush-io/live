e30c60639f0a4921638e6b4fdec25cee650bbfec
Tamas Nepusz <ntamas@gmail.com>
2025-03-09 16:42:58 +0100

    feat: RSSI indicator now supports two channels

---
+++ src/views/uavs/RSSIIndicator.tsx
@@ -0,0 +1,69 @@
+import { makeStyles, type Theme } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import React from 'react';
+
+import StatusPill from '~/components/StatusPill';
+import { getSemanticsForRSSI } from '~/model/enums';
+import { formatRSSI } from '~/utils/formatting';
+
+export type RSSIIndicatorProps = Readonly<{
+  className: string;
+  rssi: number[];
+}>;
+
+const useStyles = makeStyles((theme: Theme) => ({
+  group: {
+    display: 'inline-flex',
+    flexDirection: 'row',
+  },
+  pill: {
+    verticalAlign: 'text-top',
+    transform: 'translateY(-1px)',
+  },
+  pillMargin: {
+    margin: theme.spacing(0, 0.5),
+  },
+}));
+
+/**
+ * RSSI indicator component optimized to show one or two RSSI values at most,
+ * in a nice compact split-pill layout.
+ */
+export const RSSIIndicator = ({
+  className,
+  rssi,
+}: RSSIIndicatorProps): JSX.Element => {
+  const classes = useStyles();
+  if (rssi.length < 2) {
+    // Show only one RSSI value in a full pill
+    return (
+      <StatusPill
+        inline
+        className={clsx(className, classes.pill, classes.pillMargin)}
+        status={getSemanticsForRSSI(rssi[0])}
+      >
+        {formatRSSI(rssi[0])}
+      </StatusPill>
+    );
+  } else {
+    // Show two RSSI values in a split pill
+    return (
+      <div className={clsx(className, classes.group, classes.pillMargin)}>
+        <StatusPill
+          className={classes.pill}
+          position='left'
+          status={getSemanticsForRSSI(rssi[0])}
+        >
+          {formatRSSI(rssi[0])}
+        </StatusPill>
+        <StatusPill
+          className={classes.pill}
+          position='right'
+          status={getSemanticsForRSSI(rssi[1])}
+        >
+          {formatRSSI(rssi[1])}
+        </StatusPill>
+      </div>
+    );
+  }
+};
