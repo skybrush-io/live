b187830fb6dc773bc8a195ba743ea4c68774789f
Tamas Nepusz <ntamas@gmail.com>
2025-03-17 19:09:55 +0100

    feat: added developer mode hotkey; will be used later to enable temporary stuff that is needed during development

--- src/features/session/actions.js
+++
@@ -1,7 +0,0 @@
-import { setBroadcast } from './slice';
-import { isBroadcast } from './selectors';
-
-export const toggleBroadcast = () => (dispatch, getState) => {
-  const state = getState();
-  dispatch(setBroadcast(!isBroadcast(state)));
-};
