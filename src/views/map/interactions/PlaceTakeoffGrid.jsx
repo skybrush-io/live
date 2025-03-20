19b3ee134a6b682f15c9725bdf86f8fb838af18e
István Donkó <istvan.donko@gmail.com>
2025-03-20 14:40:13 +0100

    WIP: TakeoffGrid improvement

--- src/views/map/interactions/PlaceTakeoffGrid.jsx
+++ src/views/map/interactions/PlaceTakeoffGrid.jsx
@@ -12,64 +12,64 @@ import { interaction } from '@collmot/ol-react';
 import * as Condition from '../conditions';
 import { lonLatFromMapViewCoordinate } from '~/utils/geography';
 import { takeoffTriangle } from '../layers/mission-info';
+import memoizeOne from 'memoize-one';
 
-const PlaceTakeoffGrid = ({ takeoffGrid, updateHomePositions }) => {
-  // const takeoffTriangleParameters = {
-  //   fill: fill(Colors.markers.takeoff),
-  //   points: 3,
-  //   radius: 6,
-  //   stroke: blackVeryThinOutline,
-  // };
+/*
+ * Memoized function that only recalculates the takeoff triangle styles if the
+ * previous results would have an error of  and reuses them otherwise.
+ */
+const getTakeoffTriangleStyles = memoizeOne(
+  (coordinates, resolution) => {
+    console.log({ resolution });
+    const triangleStyle = new Style({ image: takeoffTriangle });
 
-  // const takeoffTriangleWithDisplacement = (displacement) =>
-  //   new Style({
-  //     image: new RegularShape({ ...takeoffTriangleParameters, displacement }),
-  //   });
+    return coordinates.map((displacement) => {
+      const style = triangleStyle.clone();
+      style.getImage().setDisplacement(displacement.map((d) => d / resolution));
+      return style;
+    });
+  },
+  ([prevCoordinates, prevResolution], [nextCoordinates, nextResolution]) =>
+    prevCoordinates === nextCoordinates &&
+    Math.abs(prevResolution - nextResolution) < 1e-4
+);
 
-  const tmpstyle = new Style({ image: takeoffTriangle });
-
-  const takeoffTriangleWithDisplacement = (displacement) => {
-    const sajt = tmpstyle.clone();
-    sajt.getImage().setDisplacement(displacement);
-    return sajt;
-  };
-
-  return (
-    <interaction.Draw
-      key='TakeoffGrid'
-      type='Point'
-      condition={Condition.primaryAction}
-      // PERF: Generating a whole list of styles with displacements is
-      //       probably not the best approach, maybe have a single style
-      //       and a custom interaction that has a MultiPoint sketch feature?
-      style={(f, r) => {
-        const coords = f.getGeometry().getCoordinates();
-        const scale = getPointResolution('EPSG:3857', 1, coords);
-        return takeoffGrid.coordinates.map((displacement) =>
-          takeoffTriangleWithDisplacement(
-            displacement.map((d) => d * (1 / scale / r))
+const PlaceTakeoffGrid = ({ takeoffGrid, updateHomePositions }) => (
+  <interaction.Draw
+    key='TakeoffGrid'
+    type='Point'
+    condition={Condition.primaryAction}
+    // PERF: Generating a whole list of styles with displacements is
+    //       probably not the best approach, maybe have a single style
+    //       and a custom interaction that has a MultiPoint sketch feature?
+    style={(feature, viewResolution) =>
+      getTakeoffTriangleStyles(
+        takeoffGrid.coordinates,
+        getPointResolution(
+          'EPSG:3857',
+          viewResolution,
+          feature.getGeometry().getCoordinates()
+        )
+      )
+    }
+    onDrawEnd={(event) => {
+      const [cx, cy] = event.feature.getGeometry().getCoordinates();
+      const pointResolution = getPointResolution('EPSG:3857', 1, [cx, cy]);
+      const rotation = event.target.getMap().getView().getRotation();
+      const [sr, cr] = [Math.sin(rotation), Math.cos(rotation)];
+      updateHomePositions(
+        takeoffGrid.coordinates
+          .map(([dx, dy]) =>
+            lonLatFromMapViewCoordinate([
+              cx + (dx * cr + dy * -sr) / pointResolution,
+              cy + (dx * sr + dy * cr) / pointResolution,
+            ])
           )
-        );
-      }}
-      onDrawEnd={(event) => {
-        const rotation = event.target.getMap().getView().getRotation();
-        const [cx, cy] = event.feature.getGeometry().getCoordinates();
-        const scale = getPointResolution('EPSG:3857', 1, [cx, cy]);
-        const [sr, cr] = [Math.sin(rotation), Math.cos(rotation)];
-        updateHomePositions(
-          takeoffGrid.coordinates
-            .map(([dx, dy]) =>
-              lonLatFromMapViewCoordinate([
-                cx + (dx * cr + dy * -sr) / scale,
-                cy + (dx * sr + dy * cr) / scale,
-              ])
-            )
-            .map(([lon, lat]) => ({ lon, lat }))
-        );
-      }}
-    />
-  );
-};
+          .map(([lon, lat]) => ({ lon, lat }))
+      );
+    }}
+  />
+);
 
 PlaceTakeoffGrid.propTypes = {
   takeoffGrid: PropTypes.object,
