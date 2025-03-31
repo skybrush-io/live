99ae1645bd839852e7fce4af33bff69b14ebcc5c
Tamas Nepusz <ntamas@gmail.com>
2025-03-31 23:54:23 +0200

    refactor: list and grid view are now both virtualized

---
+++ src/views/uavs/constants.ts
@@ -0,0 +1,17 @@
+/**
+ * Width of a single item in the grid-based UAV layout.
+ *
+ * Should not be smaller than the hardcoded width of the status pill, which is
+ * 80px at the moment.
+ */
+export const GRID_ITEM_WIDTH = 80;
+
+/**
+ * Height of a single row in the grid-based UAV layout.
+ */
+export const GRID_ROW_HEIGHT = 80;
+
+/**
+ * Height of the sort/filter header.
+ */
+export const HEADER_HEIGHT = 38;
