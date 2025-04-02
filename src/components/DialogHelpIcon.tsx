00b25cf69b2120a58383afd9a61478af50ec664c
Peter Volf <do.volfp@gmail.com>
2025-04-02 14:38:04 +0200

    feat: add a DialogHelpIcon component

---
+++ src/components/DialogHelpIcon.tsx
@@ -0,0 +1,31 @@
+import { makeStyles } from '@material-ui/core/styles';
+import HelpIcon from '@material-ui/icons/Help';
+import React from 'react';
+
+// @ts-ignore
+import Tooltip from '@skybrush/mui-components/lib/Tooltip';
+
+const useStyles = makeStyles((theme) => ({
+  iconContainer: {
+    flexGrow: 1,
+    padding: theme.spacing(1),
+  },
+}));
+
+type Props = {
+  content: string;
+};
+
+function DialogHelpIcon(props: Props) {
+  const { content } = props;
+  const styles = useStyles();
+  return (
+    <div className={styles.iconContainer}>
+      <Tooltip content={content}>
+        <HelpIcon />
+      </Tooltip>
+    </div>
+  );
+}
+
+export default DialogHelpIcon;
