71e1fa7931dabdddd4d0143662724e2c6bc0ff83
Tamas Nepusz <ntamas@gmail.com>
2025-04-02 23:57:55 +0200

    fix: make space for battery indicators in the UAV grid again

--- src/views/uavs/constants.ts
+++ src/views/uavs/constants.ts
@@ -9,7 +9,7 @@ export const GRID_ITEM_WIDTH = 80;
 /**
  * Height of a single row in the grid-based UAV layout.
  */
-export const GRID_ROW_HEIGHT = 80;
+export const GRID_ROW_HEIGHT = 100;
 
 /**
  * Height of the sort/filter header.
