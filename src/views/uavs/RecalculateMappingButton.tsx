d33c112d89637bafe69c7717f2ff0a38c2e663d6
Tamas Nepusz <ntamas@gmail.com>
2025-04-07 00:00:35 +0200

    refactor: get rid of a warning in the rendering of RecalculateMappingButton

--- src/views/uavs/RecalculateMappingButton.tsx
+++ src/views/uavs/RecalculateMappingButton.tsx
@@ -1,44 +1,39 @@
-import type { TFunction } from 'i18next';
 import React from 'react';
-import { connect } from 'react-redux';
-import { withTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 
 import Button, { type ButtonProps } from '@material-ui/core/Button';
 
 import { recalculateMapping } from '~/features/mission/actions';
 import { hasNonemptyMappingSlot } from '~/features/mission/selectors';
 import AutoFix from '~/icons/AutoFix';
-import type { RootState } from '~/store/reducers';
+import type { AppDispatch } from '~/store/reducers';
 
-type RecalculateMappingButtonProps = ButtonProps &
-  Readonly<{
-    hasNonemptyMappingSlot: boolean;
-    t: TFunction;
-  }>;
+type RecalculateMappingButtonProps = Omit<ButtonProps, 'onClick'>;
 
 /**
  * Button that allows the user to recalculate the current mapping from scratch
  * based on the current positions of the drones and their takeoff positions.
  */
-const RecalculateMappingButton = ({
-  hasNonemptyMappingSlot,
-  t,
-  ...rest
-}: RecalculateMappingButtonProps): JSX.Element => (
-  <Button startIcon={<AutoFix />} {...rest}>
-    {hasNonemptyMappingSlot
-      ? t('recalculateMappingButton.recalculateMapping')
-      : t('recalculateMappingButton.findOptimalMapping')}
-  </Button>
-);
+const RecalculateMappingButton = (
+  props: RecalculateMappingButtonProps
+): JSX.Element => {
+  const { t } = useTranslation();
+  const hasNonempty = useSelector(hasNonemptyMappingSlot);
+  const dispatch: AppDispatch = useDispatch();
+  return (
+    <Button
+      startIcon={<AutoFix />}
+      onClick={() => {
+        dispatch(recalculateMapping() as any);
+      }}
+      {...props}
+    >
+      {hasNonempty
+        ? t('recalculateMappingButton.recalculateMapping')
+        : t('recalculateMappingButton.findOptimalMapping')}
+    </Button>
+  );
+};
 
-export default connect(
-  // mapStateToProps
-  (state: RootState) => ({
-    hasNonemptyMappingSlot: hasNonemptyMappingSlot(state),
-  }),
-  // mapDispatchToProps
-  {
-    onClick: recalculateMapping,
-  }
-)(withTranslation()(RecalculateMappingButton));
+export default RecalculateMappingButton;
