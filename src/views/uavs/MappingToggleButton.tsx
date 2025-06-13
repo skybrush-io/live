0863300168788c741799b1baa97957e21beebe3f
Peter Volf <do.volfp@gmail.com>
2025-06-13 12:58:34 +0200

    chore: some JS to TS convesion

--- src/views/uavs/MappingToggleButton.tsx
+++ src/views/uavs/MappingToggleButton.tsx
@@ -1,39 +1,42 @@
 import Mapping from '@mui/icons-material/FormatLineSpacing';
-import PropTypes from 'prop-types';
 import React from 'react';
-import { withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import { connect } from 'react-redux';
 
 import ToggleButton from '~/components/ToggleButton';
 import { TooltipWithContainerFromContext as Tooltip } from '~/containerContext';
 import { isShowingMissionIds } from '~/features/settings/selectors';
 import { toggleMissionIds } from '~/features/settings/slice';
+import { type RootState } from '~/store/reducers';
+
+type MappingToggleButtonProps = {
+  selected: boolean;
+  onChange: (event: React.MouseEvent<HTMLElement>) => void;
+};
 
 /**
  * Toggle button that indicates whether we are primarily showing UAV IDs or
  * mission IDs in the application.
  */
-const MappingToggleButton = ({ selected, onChange, t }) => (
-  <Tooltip content={t('mappingToggleButton.sortByMissionID')}>
-    <ToggleButton value='missionIds' selected={selected} onChange={onChange}>
-      <Mapping />
-    </ToggleButton>
-  </Tooltip>
-);
+function MappingToggleButton({ selected, onChange }: MappingToggleButtonProps) {
+  const { t } = useTranslation();
 
-MappingToggleButton.propTypes = {
-  selected: PropTypes.bool,
-  onChange: PropTypes.func,
-  t: PropTypes.func,
-};
+  return (
+    <Tooltip content={t('mappingToggleButton.sortByMissionID')}>
+      <ToggleButton value='missionIds' selected={selected} onChange={onChange}>
+        <Mapping />
+      </ToggleButton>
+    </Tooltip>
+  );
+}
 
 export default connect(
   // mapStateToProps
-  (state) => ({
+  (state: RootState) => ({
     selected: isShowingMissionIds(state),
   }),
   // mapDispatchToProps
   {
     onChange: toggleMissionIds,
   }
-)(withTranslation()(MappingToggleButton));
+)(MappingToggleButton);
