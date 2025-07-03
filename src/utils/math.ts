89146a4ac367941c0a8d76a2ecdb7a34f10032bd
István Donkó <istvan.donko@gmail.com>
2025-07-03 13:47:49 +0200

    WIP: concaveman

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -5,6 +5,8 @@ import range from 'lodash-es/range';
 
 import type { Point, LineString, Polygon } from 'geojson';
 import * as TurfHelpers from '@turf/helpers';
+
+import concaveman from 'concaveman';
 import monotoneConvexHull2D from 'monotone-convex-hull-2d';
 import { err, ok, type Result } from 'neverthrow';
 
@@ -304,8 +306,12 @@ export function closePolygon(poly: Coordinate2D[]): void {
 export const convexHull2D = <C extends Coordinate2D | EasNor | LonLat>(
   coordinates: C[]
 ): C[] =>
-  // NOTE: Bang justified by `monotoneConvexHull2D` returning an index subset
-  monotoneConvexHull2D(coordinates).map((index) => coordinates[index]!);
+  true
+    ? monotoneConvexHull2D(coordinates).map((index) => coordinates[index]!)
+    : coordinates.length > 2
+      ? // NOTE: Bang justified by `monotoneConvexHull2D` returning an index subset
+        (concaveman(coordinates) as any)
+      : coordinates;
 
 /**
  * Creates an appropriate Turf.js geometry from the given list of coordinates.
