bdfb53f03ebb82a98efdd1e989152a639345055c
Peter Volf <do.volfp@gmail.com>
2025-07-30 10:26:34 +0200

    fix: make sure ToggleButton never accidentally has a border

--- src/components/ToggleButton.tsx
+++ src/components/ToggleButton.tsx
@@ -5,5 +5,5 @@ import ToggleButton_ from '@mui/material/ToggleButton';
  * Borderless variant of the Material-UI toggle button.
  */
 export default styled(ToggleButton_)({
-  border: 0,
+  border: 'none !important',
 });
