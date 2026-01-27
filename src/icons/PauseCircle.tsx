d5fffebb7d84f9b0466448fe357fd4364181ffb0
Peter Volf <do.volfp@gmail.com>
2026-01-27 12:30:39 +0100

    feat: add new pause and play icons (from heroicons)

---
+++ src/icons/PauseCircle.tsx
@@ -0,0 +1,20 @@
+import { createSvgIcon } from '@mui/material/utils';
+
+// Source: https://heroicons.com/
+export default createSvgIcon(
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 24 24'
+    strokeWidth={1.5}
+    stroke='currentColor'
+    className='size-6'
+  >
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      d='M14.25 9v6m-4.5 0V9M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'
+    />
+  </svg>,
+  'PauseCircle'
+);
