7b8c6d1ca854f0d9163d2ba3eb0e603d7529476d
István Donkó <istvan.donko@gmail.com>
2026-02-11 18:01:48 +0100

    refactor: split up parts of `addCollectiveRTH`

--- src/features/collective-rth/actions.ts
+++ src/features/collective-rth/actions.ts
@@ -1,10 +1,12 @@
+import isObject from 'lodash-es/isObject';
+
 import { getBase64ShowBlob } from '~/features/show/selectors';
 import messageHub from '~/message-hub';
 import type { AppThunk } from '~/store/reducers';
 import { writeBlobToFile } from '~/utils/filesystem';
 
 import { selectTransformedShowBlob } from './selectors';
-import { setResult } from './slice';
+import { setResult, type StatEntry, type TransformationResult } from './slice';
 
 export type OptionalCollectiveRTHParameters = {
   minDistance?: number;
@@ -15,6 +17,73 @@ export type OptionalCollectiveRTHParameters = {
 
 export type CollectiveRTHParameters = Required<OptionalCollectiveRTHParameters>;
 
+// TODO: Replace this when the message type gets promoted from experimental
+//       status and becomes defined in `@skybrush/flockwave-spec`
+type Response_XSHOWCRTHPLAN = Pick<
+  TransformationResult,
+  'show' | 'showDuration' | 'stats'
+>;
+
+function validateStatEntry(stat: unknown): asserts stat is StatEntry {
+  if (!isObject(stat)) {
+    throw new TypeError(
+      'Invalid stat in response to collective RTH transformation.'
+    );
+  }
+
+  if (!('time' in stat) || typeof stat.time !== 'number') {
+    throw new TypeError(
+      'Invalid time in stat in response to collective RTH transformation.'
+    );
+  }
+
+  if (!('duration' in stat) || typeof stat.duration !== 'number') {
+    throw new TypeError(
+      'Invalid duration in stat in response to collective RTH transformation.'
+    );
+  }
+
+  if (!('showDuration' in stat) || typeof stat.showDuration !== 'number') {
+    throw new TypeError(
+      'Invalid showDuration in stat in response to collective RTH transformation.'
+    );
+  }
+}
+
+function validateResponse(
+  resp: unknown
+): asserts resp is Response_XSHOWCRTHPLAN {
+  if (!isObject(resp)) {
+    throw new TypeError('Invalid response from collective RTH transformation.');
+  }
+
+  if (!('show' in resp) || typeof resp.show !== 'string') {
+    throw new TypeError(
+      'Invalid show response from collective RTH transformation.'
+    );
+  }
+
+  if (!('showDuration' in resp) || typeof resp.showDuration !== 'number') {
+    throw new TypeError(
+      'Invalid showDuration response from collective RTH transformation.'
+    );
+  }
+
+  if (!('stats' in resp) || !Array.isArray(resp.stats)) {
+    throw new TypeError(
+      'Invalid stats response from collective RTH transformation.'
+    );
+  }
+
+  if (resp.stats.length === 0) {
+    throw new TypeError(
+      'No stats returned from collective RTH transformation.'
+    );
+  }
+
+  resp.stats.forEach(validateStatEntry);
+}
+
 export const addCollectiveRTH =
   (params?: CollectiveRTHParameters): AppThunk =>
   async (dispatch, getState): Promise<void> => {
@@ -23,79 +92,21 @@ export const addCollectiveRTH =
     dispatch(setResult({ state: 'loading' }));
 
     try {
-      const { show, showDuration, stats } =
-        await messageHub.query.addCollectiveRTH(base64ShowBlob, {
-          min_distance: params?.minDistance,
-          time_resolution: params?.timeResolution,
-          velocity_xy: params?.horizontalVelocity,
-          velocity_z: params?.verticalVelocity,
-        });
+      const response = await messageHub.query.addCollectiveRTH(base64ShowBlob, {
+        min_distance: params?.minDistance,
+        time_resolution: params?.timeResolution,
+        velocity_xy: params?.horizontalVelocity,
+        velocity_z: params?.verticalVelocity,
+      });
 
-      if (typeof show !== 'string') {
-        throw new TypeError(
-          'Invalid show response from collective RTH transformation.'
-        );
-      }
+      validateResponse(response);
 
-      if (typeof showDuration !== 'number') {
-        throw new TypeError(
-          'Invalid showDuration response from collective RTH transformation.'
-        );
-      }
-
-      if (!Array.isArray(stats)) {
-        throw new TypeError(
-          'Invalid stats response from collective RTH transformation.'
-        );
-      }
-
-      if (stats.length === 0) {
-        throw new TypeError(
-          'No stats returned from collective RTH transformation.'
-        );
-      }
-
-      let firstTime: number = Number.POSITIVE_INFINITY;
-      let lastTime: number = Number.NEGATIVE_INFINITY;
-
-      for (const stat of stats) {
-        if (typeof stat !== 'object') {
-          throw new TypeError(
-            'Invalid stat in response to collective RTH transformation.'
-          );
-        }
-
-        if (typeof stat.time !== 'number') {
-          throw new TypeError(
-            'Invalid time in stat in response to collective RTH transformation.'
-          );
-        }
-
-        if (typeof stat.duration !== 'number') {
-          throw new TypeError(
-            'Invalid duration in stat in response to collective RTH transformation.'
-          );
-        }
-
-        if (typeof stat.showDuration !== 'number') {
-          throw new TypeError(
-            'Invalid showDuration in stat in response to collective RTH transformation.'
-          );
-        }
-
-        firstTime = Math.min(firstTime, stat.time as number);
-        lastTime = Math.max(lastTime, stat.time as number);
-      }
+      const times = response.stats.map(({ time }) => time);
+      const firstTime = Math.min(...times);
+      const lastTime = Math.max(...times);
 
       dispatch(
-        setResult({
-          state: 'success',
-          show,
-          showDuration,
-          stats,
-          firstTime,
-          lastTime,
-        })
+        setResult({ state: 'success', ...response, firstTime, lastTime })
       );
     } catch (error) {
       console.warn('addCollectiveRTH failed with error:', error);
