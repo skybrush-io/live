80c08779f5d4d673857986e376c3a0ec1f33b2de
Peter Volf <do.volfp@gmail.com>
2026-03-17 15:06:26 +0100

    feat: add tooltips to pro control buttons, refactor prerequisite resolution and UI to avoid code multiplication, closes #144

--- src/views/show-control/ProControlButtonGroup.tsx
+++ src/views/show-control/ProControlButtonGroup.tsx
@@ -1,13 +1,16 @@
 import PauseCircleOutlined from '@mui/icons-material/PauseCircleOutlined';
 import PlayCircleOutlined from '@mui/icons-material/PlayCircleOutlined';
 import Box from '@mui/material/Box';
-import { makeStyles } from '@skybrush/app-theme-mui';
 import { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import { connect } from 'react-redux';
 
+import { makeStyles } from '@skybrush/app-theme-mui';
+import { Tooltip } from '@skybrush/mui-components';
+
 import ColoredButton from '~/components/ColoredButton';
 import Colors from '~/components/colors';
+import PrerequisiteList from '~/components/PrerequisiteList';
 import {
   hasLicenseWithProFeatures,
   supportsSuspendResumeCRTH,
@@ -17,11 +20,47 @@ import {
   startCollectiveRTH,
   suspendShow,
 } from '~/features/show/actions';
-import { selectIsCollectiveRTHTriggered } from '~/features/show/selectors';
+import {
+  selectCollectiveRTHPlanSummary,
+  selectIsCollectiveRTHTriggered,
+} from '~/features/show/selectors';
 import { showWarning } from '~/features/snackbar/actions';
+import {
+  useConstPrerequisites,
+  type Prerequisite,
+} from '~/hooks/useConstPrerequisites';
+import { tt } from '~/i18n';
 import HomeCircleOutlined from '~/icons/HomeCircleOutlined';
 import { type RootState } from '~/store/reducers';
 import { Workbench } from '~/workbench';
+
+const GENERAL_PREREQUISITES: readonly Prerequisite[] = Object.freeze([
+  {
+    selector: (state: RootState) => hasLicenseWithProFeatures(state) ?? false,
+    message: tt('proControlButtonGroup.prerequisites.proFeaturesEnabled'),
+  },
+  {
+    selector: supportsSuspendResumeCRTH,
+    message: tt(
+      'proControlButtonGroup.prerequisites.supportsSuspendResumeCRTH'
+    ),
+  },
+  {
+    selector: (state: RootState) => !selectIsCollectiveRTHTriggered(state),
+    message: tt(
+      'proControlButtonGroup.prerequisites.collectiveRTHNotTriggered'
+    ),
+  },
+]);
+
+const CRTH_PREREQUISITES: readonly Prerequisite[] = Object.freeze([
+  {
+    selector: (state: RootState) =>
+      selectCollectiveRTHPlanSummary(state).isValid,
+    message: tt('proControlButtonGroup.prerequisites.hasCollectiveRTHPlan'),
+  },
+  ...GENERAL_PREREQUISITES,
+]);
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -29,18 +68,16 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'row',
     flex: 1,
   },
-  button: {
+  buttonWrapper: {
+    display: 'flex',
     flex: 1,
     margin: theme.spacing(0.5),
     lineHeight: '1 !important',
   },
+  button: {
+    flex: 1,
+  },
 }));
-
-type StateProps = {
-  isCollectiveRTHTriggered: boolean;
-  proFeaturesEnabled?: boolean;
-  supportsSuspendResumeCRTH: boolean;
-};
 
 type DispatchProps = {
   resumeShow: () => void;
@@ -48,71 +85,87 @@ type DispatchProps = {
   suspendShow: () => void;
 };
 
-type Props = StateProps & DispatchProps;
+type Props = DispatchProps;
 
 const ProControlButtonGroup = (props: Props) => {
-  const {
-    isCollectiveRTHTriggered,
-    proFeaturesEnabled,
-    resumeShow,
-    startCollectiveRTH,
-    supportsSuspendResumeCRTH,
-    suspendShow,
-  } = props;
+  const { resumeShow, startCollectiveRTH, suspendShow } = props;
   const workbench = useContext(Workbench);
   const classes = useStyles();
   const { t } = useTranslation();
 
-  const actionsDisabled =
-    isCollectiveRTHTriggered ||
-    !proFeaturesEnabled ||
-    !supportsSuspendResumeCRTH;
+  const { prerequisites, prerequisitesFulfilled } = useConstPrerequisites(
+    GENERAL_PREREQUISITES
+  );
+  const {
+    prerequisites: crthPrerequisites,
+    prerequisitesFulfilled: crthPrerequisitesFulfilled,
+  } = useConstPrerequisites(CRTH_PREREQUISITES);
 
   return (
     <Box className={classes.root}>
-      <ColoredButton
-        className={classes.button}
-        color={Colors.positionHold}
-        disabled={actionsDisabled}
-        icon={<PauseCircleOutlined fontSize='inherit' />}
-        onClick={suspendShow}
+      <Tooltip
+        disabled={prerequisitesFulfilled}
+        content={<PrerequisiteList prerequisites={prerequisites} />}
       >
-        {t('proControlButtonGroup.suspend')}
-      </ColoredButton>
-      <ColoredButton
-        className={classes.button}
-        color={Colors.success}
-        disabled={actionsDisabled}
-        icon={<PlayCircleOutlined fontSize='inherit' />}
-        onClick={resumeShow}
+        <div tabIndex={0} className={classes.buttonWrapper}>
+          <ColoredButton
+            className={classes.button}
+            color={Colors.positionHold}
+            disabled={!prerequisitesFulfilled}
+            icon={<PauseCircleOutlined fontSize='inherit' />}
+            onClick={suspendShow}
+          >
+            {t('proControlButtonGroup.suspend')}
+          </ColoredButton>
+        </div>
+      </Tooltip>
+      <Tooltip
+        disabled={prerequisitesFulfilled}
+        content={<PrerequisiteList prerequisites={prerequisites} />}
       >
-        {t('proControlButtonGroup.resume')}
-      </ColoredButton>
-      <ColoredButton
-        className={classes.button}
-        color={Colors.seriousWarning}
-        disabled={actionsDisabled}
-        icon={<HomeCircleOutlined fontSize='inherit' />}
-        onClick={() => {
-          startCollectiveRTH();
-          if (!workbench.bringToFront('collectiveRTH')) {
-            showWarning(t('proControlButtonGroup.collectiveRTHPanelNotFound'));
-          }
-        }}
+        <div tabIndex={0} className={classes.buttonWrapper}>
+          <ColoredButton
+            className={classes.button}
+            color={Colors.success}
+            disabled={!prerequisitesFulfilled}
+            icon={<PlayCircleOutlined fontSize='inherit' />}
+            onClick={resumeShow}
+          >
+            {t('proControlButtonGroup.resume')}
+          </ColoredButton>
+        </div>
+      </Tooltip>
+      <Tooltip
+        disabled={crthPrerequisitesFulfilled}
+        content={<PrerequisiteList prerequisites={crthPrerequisites} />}
       >
-        {t('proControlButtonGroup.collectiveRTH')}
-      </ColoredButton>
+        <div tabIndex={0} className={classes.buttonWrapper}>
+          <ColoredButton
+            className={classes.button}
+            color={Colors.seriousWarning}
+            disabled={!crthPrerequisitesFulfilled}
+            icon={<HomeCircleOutlined fontSize='inherit' />}
+            onClick={() => {
+              startCollectiveRTH();
+              if (!workbench.bringToFront('collectiveRTH')) {
+                showWarning(
+                  t('proControlButtonGroup.collectiveRTHPanelNotFound')
+                );
+              }
+            }}
+          >
+            {t('proControlButtonGroup.collectiveRTH')}
+          </ColoredButton>
+        </div>
+      </Tooltip>
     </Box>
   );
 };
 
-const ConnectedProControlButtonGroup = connect(
-  (state: RootState) => ({
-    isCollectiveRTHTriggered: selectIsCollectiveRTHTriggered(state),
-    proFeaturesEnabled: hasLicenseWithProFeatures(state),
-    supportsSuspendResumeCRTH: supportsSuspendResumeCRTH(state),
-  }),
-  { resumeShow, startCollectiveRTH, suspendShow }
-)(ProControlButtonGroup);
+const ConnectedProControlButtonGroup = connect(null, {
+  resumeShow,
+  startCollectiveRTH,
+  suspendShow,
+})(ProControlButtonGroup);
 
 export default ConnectedProControlButtonGroup;
