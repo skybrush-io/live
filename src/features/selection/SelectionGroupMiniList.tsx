ace8bbfb5f9c1367ef570b038489d5fd0bb5958e
Tamas Nepusz <ntamas@gmail.com>
2026-04-13 01:28:51 +0200

    feat: selection group header button now uses MiniList

---
+++ src/features/selection/SelectionGroupMiniList.tsx
@@ -0,0 +1,47 @@
+import {
+  MiniList,
+  MiniListDivider,
+  MiniListItemButton,
+} from '@skybrush/mui-components';
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+import { useAppDispatch } from '~/store/hooks';
+import SelectionGroupMiniListItem from './SelectionGroupMiniListItem';
+import { getOrderedSelectionGroups, hasSelection } from './selectors';
+import { saveCurrentSelectionAsGroup } from './slice';
+
+const listStyle = {
+  minWidth: 150,
+};
+
+const SelectionGroupMiniList = () => {
+  const isSelectionNotEmpty = useSelector(hasSelection);
+  const selectionGroups = useSelector(getOrderedSelectionGroups);
+  const dispatch = useAppDispatch();
+  const { t } = useTranslation(undefined, {
+    keyPrefix: 'selectionGroups',
+  });
+
+  return (
+    <MiniList style={listStyle}>
+      {selectionGroups.map((group, index) => (
+        <SelectionGroupMiniListItem
+          key={group.id}
+          group={group}
+          index={index}
+        />
+      ))}
+      {selectionGroups.length > 0 && <MiniListDivider />}
+      <MiniListItemButton
+        disabled={!isSelectionNotEmpty}
+        onClick={() => {
+          dispatch(saveCurrentSelectionAsGroup());
+        }}
+        iconPreset='add'
+        primaryText={t('action.createNew')}
+      />
+    </MiniList>
+  );
+};
+
+export default SelectionGroupMiniList;
---
+++ src/features/selection/SelectionGroupMiniListItem.tsx
@@ -0,0 +1,74 @@
+import { MiniListItemButton, MiniListItemIcon } from '@skybrush/mui-components';
+import { useCallback, useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useAppDispatch } from '~/store/hooks';
+import { deleteGroup, saveCurrentSelectionAsGroup, selectGroup } from './slice';
+import type { SelectionGroup } from './types';
+
+type Props = {
+  index: number;
+  group: SelectionGroup;
+};
+
+const SelectionGroupMiniListItem = ({ index, group }: Props) => {
+  const { id, name } = group;
+  const dispatch = useAppDispatch();
+  const [recentlyUpdated, setRecentlyUpdated] = useState(false);
+  const { t } = useTranslation(undefined, {
+    keyPrefix: 'selectionGroups',
+  });
+
+  const handleSelect = useCallback(() => {
+    dispatch(selectGroup(id));
+  }, [id, dispatch]);
+
+  const handleUpdate = useCallback(() => {
+    dispatch(saveCurrentSelectionAsGroup(id));
+    setRecentlyUpdated(true);
+  }, [id, dispatch, setRecentlyUpdated]);
+
+  const handleDelete = useCallback(() => {
+    dispatch(deleteGroup(id));
+  }, [id, dispatch]);
+
+  // Revert recentlyUpdated to false after 3 seconds
+  useEffect(() => {
+    const timeoutId = recentlyUpdated
+      ? setTimeout(() => {
+          setRecentlyUpdated(false);
+        }, 3000)
+      : undefined;
+
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+    };
+  }, [recentlyUpdated, setRecentlyUpdated]);
+
+  return (
+    <MiniListItemButton
+      key={id}
+      iconPreset={
+        recentlyUpdated ? 'success' : index < 6 ? `number${index + 1}` : 'empty'
+      }
+      primaryText={recentlyUpdated ? t('updated') : name}
+      secondaryActions={
+        recentlyUpdated ? (
+          <>
+            <MiniListItemIcon preset='empty' />
+            <MiniListItemIcon preset='empty' />
+          </>
+        ) : (
+          <>
+            <MiniListItemIcon preset='update' onClick={handleUpdate} />
+            <MiniListItemIcon preset={'delete'} onClick={handleDelete} />
+          </>
+        )
+      }
+      onClick={handleSelect}
+    />
+  );
+};
+
+export default SelectionGroupMiniListItem;
