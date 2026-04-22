06b341048b77cadca5cf1cd217c693c353d01f2a
István Donkó <istvan.donko@gmail.com>
2026-04-22 12:42:16 +0200

    feat: add support for GeoTIFFs in the image layer

--- src/views/map/layers/image.jsx
+++ src/views/map/layers/image.jsx
@@ -1,14 +1,21 @@
+import { Base64 } from 'js-base64';
 import isEqual from 'lodash-es/isEqual';
-import { getPointResolution } from 'ol/proj';
+import { getPointResolution, transformExtent } from 'ol/proj';
+import { fromEPSGCode, register } from 'ol/proj/proj4';
+import proj4 from 'proj4';
 import PropTypes from 'prop-types';
 import { useCallback, useEffect, useRef } from 'react';
 import { Form, FormSpy } from 'react-final-form';
+import { useTranslation } from 'react-i18next';
 import { connect } from 'react-redux';
 
 import { layer, source } from '@collmot/ol-react';
 
 import Image from '@mui/icons-material/Image';
+import Navigation from '@mui/icons-material/Navigation';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Collapse from '@mui/material/Collapse';
 import Grid from '@mui/material/Grid';
 import InputAdornment from '@mui/material/InputAdornment';
 import Skeleton from '@mui/material/Skeleton';
@@ -23,10 +30,27 @@ import {
 } from '~/components/forms';
 import { setLayerParametersById } from '~/features/map/layers';
 import { getMapViewCenterPosition } from '~/selectors/map';
+import { mapReferenceRequestSignal, mapViewToExtentSignal } from '~/signals';
 import { readFileAsDataURL } from '~/utils/files';
 import { mapViewCoordinateFromLonLat } from '~/utils/geography';
 import { toRadians } from '~/utils/math';
 import { finite, join, positive, required } from '~/utils/validation';
+
+// TODO: Do this initialization call in a more appropriate part of the codebase!
+register(proj4);
+
+// NOTE: This is more like a quick and dirty solution for a specific use case
+//       rather than a proper generic data url to blob converter, so I defined
+//       it here locally instead of putting it into a shared utility file.
+const base64DataURLToBlob = (base64DataURL) => {
+  // NOTE: `fetch` cannot be used due to the limit on the length of the URL
+  // const blob = await fetch(image.data).then((response) => response.blob());
+  const base64DataURLPattern = /^data:(?<type>.*?);base64,(?<data>.*)$/;
+  const { type, data } = base64DataURL.match(base64DataURLPattern).groups;
+  // NOTE: Alternative in case we would like to drop the `Base64` dependency:
+  // const part = Uint8Array.from(window.atob(data), (m) => m.codePointAt(0));
+  return new Blob([Base64.toUint8Array(data)], { type });
+};
 
 const getDimensions = async (source) =>
   new Promise((resolve) => {
@@ -38,11 +62,13 @@ const getDimensions = async (source) =>
   });
 
 const ImageLayerSettingsPresentation = ({
-  layer: { parameters },
+  layer: { id, parameters },
   mapViewCenterPosition,
   selectImage,
   updateTransform,
 }) => {
+  const { t } = useTranslation();
+
   const inputRef = useRef();
   const handleClick = useCallback(() => {
     if (inputRef.current) {
@@ -66,115 +92,175 @@ const ImageLayerSettingsPresentation = ({
     forceFormSubmission('ImageTransformEditor');
   }, []);
 
-  return (
-    <Form
-      initialValues={{ ...parameters.transform }}
-      onSubmit={updateTransform}
-    >
-      {({ handleSubmit, values }) => (
-        <form id='ImageTransformEditor' onSubmit={handleSubmit}>
-          <FormSpy
-            subscription={{ values: true }}
-            onChange={({ values: newValues }) => {
-              if (!isEqual(values, newValues)) {
-                _forceFormSubmission();
-              }
-            }}
-          />
-          <Grid container spacing={1}>
-            <Grid
-              size={6}
-              display='flex'
-              justifyContent='center'
-              alignItems='center'
-            >
-              {parameters.image.name ? (
-                <img
-                  src={parameters.image.data}
-                  style={{
-                    width: '100%',
-                    height: '10em',
-                    objectFit: 'contain',
-                  }}
-                />
-              ) : (
-                <Skeleton
-                  variant='rectangular'
-                  width='100%'
-                  height='10em'
-                  onClick={handleClick}
-                />
-              )}
-            </Grid>
-            <Grid
-              size={6}
-              display='flex'
-              flexDirection='column'
-              justifyContent='center'
-              alignItems='center'
-            >
-              <FileButton
-                ref={inputRef}
-                filter={['image/*']}
-                variant='contained'
-                startIcon={<Image />}
-                onSelected={selectImage}
-              >
-                Select
-              </FileButton>
-              <Box sx={{ p: 0.75 }} />
-              {parameters.image.name ? (
-                <>
-                  <span>{parameters.image.name}</span>
-                  <span style={{ fontStyle: 'italic' }}>
-                    {parameters.image.dimensions.width}&nbsp;×&nbsp;
-                    {parameters.image.dimensions.height}&nbsp;px
-                  </span>
-                </>
-              ) : (
-                'Please select an image!'
-              )}
-            </Grid>
-            <Grid size={6}>
-              <LatitudeField
-                size='small'
-                name='position.lat'
-                label='Latitude of center'
-              />
-            </Grid>
-            <Grid size={6}>
-              <LongitudeField
-                size='small'
-                name='position.lon'
-                label='Longitude of center'
-              />
-            </Grid>
-            <Grid size={6}>
-              <HeadingField size='small' name='angle' label='Angle' />
-            </Grid>
-            <Grid size={6}>
-              <TextField
-                type='number'
-                fieldProps={{ validate: join([required, finite, positive]) }}
-                slotProps={{
-                  htmlInput: { step: 0.1 },
-                  input: {
-                    endAdornment: (
-                      <InputAdornment position='end'>cm/px</InputAdornment>
-                    ),
-                  },
-                }}
-                size='small'
-                name='scale'
-                label='Scale'
-                variant='filled'
-              />
-            </Grid>
-          </Grid>
-          <input hidden type='submit' />
-        </form>
+  const filePicker = (
+    <>
+      <FileButton
+        ref={inputRef}
+        filter={['image/*']}
+        variant='contained'
+        startIcon={<Image />}
+        onSelected={selectImage}
+      >
+        Select
+      </FileButton>
+      <Box sx={{ p: 0.75 }} />
+      {parameters.image.name ? (
+        <>
+          <span>{parameters.image.name}</span>
+          {parameters.image.dimensions && (
+            <span style={{ fontStyle: 'italic' }}>
+              {parameters.image.dimensions.width}&nbsp;×&nbsp;
+              {parameters.image.dimensions.height}&nbsp;px
+            </span>
+          )}
+        </>
+      ) : (
+        'Please select an image!'
       )}
-    </Form>
+    </>
+  );
+
+  const isGeoTIFF = parameters.image.data.startsWith('data:image/tiff');
+
+  const navigateToGeoTIFFLocation = useCallback(() => {
+    mapReferenceRequestSignal.dispatch(async (map) => {
+      const source = map
+        .getLayers()
+        .getArray()
+        .find((l) => l.get('id') === id)
+        ?.getSource?.();
+      if (source) {
+        const projection = await fromEPSGCode(source.getProjection().getCode());
+        const view = await source.getView();
+        mapViewToExtentSignal.dispatch(
+          transformExtent(view.extent, projection, 'EPSG:3857')
+        );
+      }
+    });
+  }, [id]);
+
+  return (
+    <>
+      <Collapse in={!isGeoTIFF}>
+        <Form
+          initialValues={{ ...parameters.transform }}
+          onSubmit={updateTransform}
+        >
+          {({ handleSubmit, values }) => (
+            <form id='ImageTransformEditor' onSubmit={handleSubmit}>
+              <FormSpy
+                subscription={{ values: true }}
+                onChange={({ values: newValues }) => {
+                  if (!isEqual(values, newValues)) {
+                    _forceFormSubmission();
+                  }
+                }}
+              />
+              <Grid container spacing={1}>
+                <Grid
+                  size={6}
+                  display='flex'
+                  justifyContent='center'
+                  alignItems='center'
+                >
+                  {parameters.image.name ? (
+                    <img
+                      src={parameters.image.data}
+                      style={{
+                        width: '100%',
+                        height: '10em',
+                        objectFit: 'contain',
+                      }}
+                    />
+                  ) : (
+                    <Skeleton
+                      variant='rectangular'
+                      width='100%'
+                      height='10em'
+                      onClick={handleClick}
+                    />
+                  )}
+                </Grid>
+                <Grid
+                  size={6}
+                  display='flex'
+                  flexDirection='column'
+                  justifyContent='center'
+                  alignItems='center'
+                >
+                  {filePicker}
+                </Grid>
+                <Grid size={6}>
+                  <LatitudeField
+                    size='small'
+                    name='position.lat'
+                    label='Latitude of center'
+                  />
+                </Grid>
+                <Grid size={6}>
+                  <LongitudeField
+                    size='small'
+                    name='position.lon'
+                    label='Longitude of center'
+                  />
+                </Grid>
+                <Grid size={6}>
+                  <HeadingField size='small' name='angle' label='Angle' />
+                </Grid>
+                <Grid size={6}>
+                  <TextField
+                    type='number'
+                    fieldProps={{
+                      validate: join([required, finite, positive]),
+                    }}
+                    slotProps={{
+                      htmlInput: { step: 0.1 },
+                      input: {
+                        endAdornment: (
+                          <InputAdornment position='end'>cm/px</InputAdornment>
+                        ),
+                      },
+                    }}
+                    size='small'
+                    name='scale'
+                    label='Scale'
+                    variant='filled'
+                  />
+                </Grid>
+              </Grid>
+              <input hidden type='submit' />
+            </form>
+          )}
+        </Form>
+      </Collapse>
+      <Collapse in={isGeoTIFF}>
+        <Grid container spacing={1}>
+          <Grid
+            size={6}
+            display='flex'
+            justifyContent='center'
+            alignItems='center'
+          >
+            <Button
+              variant='contained'
+              endIcon={<Navigation />}
+              onClick={navigateToGeoTIFFLocation}
+            >
+              {t('general.action.navigate')}
+            </Button>
+          </Grid>
+          <Grid
+            size={6}
+            display='flex'
+            flexDirection='column'
+            justifyContent='center'
+            alignItems='center'
+          >
+            {filePicker}
+          </Grid>
+        </Grid>
+      </Collapse>
+    </>
   );
 };
 
@@ -193,29 +279,32 @@ export const ImageLayerSettings = connect(
   // mapDispatchToProps
   (dispatch, ownProps) => ({
     async selectImage(file) {
+      // TODO: use `localforage` to persistently store images
+      //       as blobs and `URL.createOjectURL` to load them?
       const data = await readFileAsDataURL(file);
       dispatch(
         setLayerParametersById(ownProps.layerId, {
           image: {
             data,
             name: file.name,
-            dimensions: await getDimensions(data),
+            dimensions: data.startsWith('data:image/tiff')
+              ? undefined
+              : await getDimensions(data),
           },
         })
       );
     },
     updateTransform(transform) {
-      const parsedTransform = {
-        position: {
-          lon: Number(transform.position.lon),
-          lat: Number(transform.position.lat),
-        },
-        angle: Number(transform.angle),
-        scale: Number(transform.scale),
-      };
       dispatch(
         setLayerParametersById(ownProps.layerId, {
-          transform: parsedTransform,
+          transform: {
+            position: {
+              lon: Number(transform.position.lon),
+              lat: Number(transform.position.lat),
+            },
+            angle: Number(transform.angle),
+            scale: Number(transform.scale),
+          },
         })
       );
     },
@@ -224,6 +313,7 @@ export const ImageLayerSettings = connect(
 
 const ImageLayerPresentation = ({
   layer: {
+    id,
     parameters: {
       image,
       transform: { position, angle, scale },
@@ -231,7 +321,11 @@ const ImageLayerPresentation = ({
   },
   zIndex,
 }) =>
-  position ? (
+  image.data.startsWith('data:image/tiff') ? (
+    <layer.WebGLTile properties={{ id }} zIndex={zIndex}>
+      <source.GeoTIFF sources={[{ blob: base64DataURLToBlob(image.data) }]} />
+    </layer.WebGLTile>
+  ) : position ? (
     <layer.GeoImage zIndex={zIndex}>
       <source.GeoImage
         url={image.data}
