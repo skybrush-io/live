f94640cdbb856b5bd7c08cec605c5b98333946c6
István Donkó <istvan.donko@gmail.com>
2026-05-04 08:15:11 +0200

    fix: restore UAV details panel behavior and style
    The style was broken in `a1ddc548` (MUI v5 upgrade) and the switch event
    handling was incorrect since `0ee5d408` (refactoring to TypeScript)

--- src/views/uav-details/UAVDetailsPanelSelect.tsx
+++ src/views/uav-details/UAVDetailsPanelSelect.tsx
@@ -1,26 +1,31 @@
+import capitalize from 'lodash-es/capitalize';
+import { connect } from 'react-redux';
+
 import MenuItem from '@mui/material/MenuItem';
 import Select, {
   type SelectChangeEvent,
   type SelectProps,
 } from '@mui/material/Select';
-import capitalize from 'lodash-es/capitalize';
-import { connect } from 'react-redux';
 
 import { getSelectedTabInUAVDetailsPanel } from '~/features/uavs/selectors';
 import { setSelectedTabInUAVDetailsPanel } from '~/features/uavs/slice';
-import type { RootState } from '~/store/reducers';
-
 import {
   isUAVDetailsPanelTab,
   type UAVDetailsPanelTab,
 } from '~/features/uavs/types';
+import type { AppDispatch, RootState } from '~/store/reducers';
+
 import { views } from './UAVDetailsPanelBody';
 
 /**
  * Dropdown menu for selecting a view on the UAV Details Panel.
  */
 const UAVDetailsPanelSelect = (props: SelectProps<UAVDetailsPanelTab>) => (
-  <Select style={{ width: 128, textAlign: 'center' }} {...props}>
+  <Select
+    variant='standard'
+    style={{ width: 128, textAlign: 'center' }}
+    {...props}
+  >
     {Object.keys(views).map((view) => (
       <MenuItem key={view} value={view}>
         {capitalize(view)}
@@ -34,12 +39,12 @@ export default connect(
   (state: RootState) => ({ value: getSelectedTabInUAVDetailsPanel(state) }),
 
   // mapDispatchToProps
-  {
+  (dispatch: AppDispatch) => ({
     onChange: (event: SelectChangeEvent) => {
       const { value } = event.target;
       if (isUAVDetailsPanelTab(value)) {
-        setSelectedTabInUAVDetailsPanel(value);
+        dispatch(setSelectedTabInUAVDetailsPanel(value));
       }
     },
-  }
+  })
 )(UAVDetailsPanelSelect);
