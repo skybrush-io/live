5ae594dcc1cecbcd25f2614def90892c06ef819f
Peter Volf <do.volfp@gmail.com>
2026-05-29 11:05:22 +0200

    refactor: convert objects js to ts

--- src/model/objects.ts
+++ src/model/objects.ts
@@ -4,15 +4,16 @@
  */
 
 import { notifyObjectsDeletedOnServer } from '~/features/objects/actions';
+import type { AppDispatch } from '~/store/reducers';
 
 /**
  * Handles an OBJ-DEL message from a server and updates the state of the Redux
  * store appropriately.
- *
- * @param  {Object} body  the body of the OBJ-DEL message
- * @param  {function} dispatch  the dispatch function of the Redux store
  */
-export function handleObjectDeletionMessage(body, dispatch) {
+export function handleObjectDeletionMessage(
+  body: { ids: string[] },
+  dispatch: AppDispatch
+): void {
   /* TODO(ntamas): we have to decide whether deleting the object from the client
    * is a good idea or not. In the extreme case, if a drone is removed from the
    * server due to a timeout but it comes back in a few seconds, we could be
