f278bc4ea49b070cc2ba2986673e07d4855d6171
Peter Volf <do.volfp@gmail.com>
2026-06-10 12:35:53 +0200

    chore: flight log typing and validation improvements

--- src/model/flight-logs.ts
+++ src/model/flight-logs.ts
@@ -3,15 +3,24 @@ import fromUnixTime from 'date-fns/fromUnixTime';
 import { Base64 } from 'js-base64';
 import isNil from 'lodash-es/isNil';
 
+import type {
+  FlightLog as FlockwaveFlightLog,
+  FlightLogMetadata as FlockwaveFlightLogMetadata,
+} from '@skybrush/flockwave-spec';
+
 import { FlightLogKind } from './enums';
 
 export { FlightLogKind } from './enums';
 
-export type FlightLog = {
-  kind: FlightLogKind;
-  body: string;
-  id?: string;
+export type FlightLogMetadata = {
+  id: string;
   timestamp?: number;
+  kind: FlightLogKind;
+  size?: number;
+};
+
+export type FlightLog = FlightLogMetadata & {
+  body: string;
 };
 
 const _EXTENSIONS_FOR_FLIGHT_LOG_KINDS: Record<FlightLogKind, string> = {
@@ -72,4 +81,35 @@ export function proposeFilenameForFlightLog(flightLog: FlightLog): string {
   } else {
     return `${id}_${ts}${extension}`;
   }
+}
+
+function validateFlightLogKind(value: string): FlightLogKind {
+  return (Object.values(FlightLogKind) as string[]).includes(value)
+    ? (value as FlightLogKind)
+    : FlightLogKind.UNKNOWN;
+}
+
+export function validateFlightLogMetadata(
+  metadata: FlockwaveFlightLogMetadata
+): FlightLogMetadata {
+  return {
+    id: metadata.id,
+    timestamp: metadata.timestamp,
+    kind: validateFlightLogKind(metadata.kind),
+    size: metadata.size,
+  };
+}
+
+export function validateFlockwaveFlightLog(log: FlockwaveFlightLog): FlightLog {
+  const kind = validateFlightLogKind(log.kind);
+  let body = log.body;
+  if (!(typeof body === 'string')) {
+    if (kind === FlightLogKind.UNKNOWN) {
+      body = JSON.stringify(body);
+    } else {
+      throw new Error('log.body must be a string');
+    }
+  }
+
+  return { kind, body, id: log.id, timestamp: log.timestamp, size: log.size };
 }
