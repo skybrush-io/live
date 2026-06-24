af06ce0c245ee2867f9670e0a7c6a1138c44d8d2
Tamas Nepusz <ntamas@gmail.com>
2026-06-24 18:09:32 +0200

    style: refinements to ScheduleStatusLight

--- src/components/progress/ScheduleStatusLight.tsx
+++ src/components/progress/ScheduleStatusLight.tsx
@@ -1,8 +1,9 @@
 import Stack, { type StackProps } from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
-import { Status } from '@skybrush/app-theme-mui';
+import { colorForStatus, Status } from '@skybrush/app-theme-mui';
 import {
   LabeledStatusLight,
+  Tooltip,
   type LabeledStatusLightProps,
 } from '@skybrush/mui-components';
 import { useState } from 'react';
@@ -15,12 +16,14 @@ import {
   type TimeSegment,
 } from '~/flockwave/schedule';
 
+import Box from '@mui/material/Box';
 import TimeAgo from 'react-timeago';
 import { shortTimeAgoFormatter } from '~/utils/formatting';
 import { useRefreshTimestampWhile } from './ScheduleProgressCards';
 
 type Props = {
   schedule: TimeSegment[];
+  emptyTitle?: string;
 } & StackProps &
   Pick<LabeledStatusLightProps, 'color' | 'size'>;
 
@@ -34,60 +37,79 @@ const EMPTY_SEGMENT: TimeSegment = {
  * Component that shows the status of a drone show schedule in a light-weight way,
  * with a single labeled status light only.
  */
-const ScheduleStatusLight = ({ schedule, color, size, sx, ...rest }: Props) => {
+const ScheduleStatusLight = ({
+  emptyTitle,
+  schedule,
+  color,
+  size,
+  sx,
+  ...rest
+}: Props) => {
   const { t } = useTranslation();
   const [nowMs, setNowMs] = useState(() => Date.now());
-  const segment =
-    findCurrentOrNextTimeSegment(schedule, nowMs) ??
-    schedule.at(-1) ??
-    EMPTY_SEGMENT;
-  const { progress, stage } = calculateSegmentProgress(segment, nowMs);
+  const segment = findCurrentOrNextTimeSegment(schedule, nowMs); // ?? schedule.at(-1);
+  const { progress, stage } = calculateSegmentProgress(
+    segment ?? EMPTY_SEGMENT,
+    nowMs
+  );
   useRefreshTimestampWhile(stage !== 'completed', setNowMs);
 
-  const title = t(`scheduleProgressIndicator.segment.${segment.type}.title`);
-  const timestamp =
-    stage === 'waiting'
+  const segmentType = segment ? `segment.${segment.type}` : 'empty';
+  const title = t(`scheduleProgressIndicator.${segmentType}.title`);
+  const tooltip = t(`scheduleProgressIndicator.${segmentType}.description`);
+  const timestamp = segment
+    ? stage === 'waiting'
       ? segment.startMs
       : stage === 'active'
         ? segment.endMs
-        : undefined;
+        : undefined
+    : undefined;
+  const status = segment
+    ? stage === 'waiting'
+      ? Status.WAITING
+      : stage === 'active'
+        ? Status.NEXT
+        : Status.SUCCESS
+    : Status.OFF;
+
   return (
-    <Stack direction='column' spacing={1} sx={{ ...sx }} {...rest}>
-      <Stack direction='row' spacing={2} sx={{ alignItems: 'center' }}>
-        <LabeledStatusLight
-          status={
-            stage === 'waiting'
-              ? Status.WAITING
-              : stage === 'active'
-                ? Status.NEXT
-                : Status.SUCCESS
-          }
-          color={color}
-          size={size}
-          sx={{ flex: 1 }}
-        >
-          {title}
-        </LabeledStatusLight>
-        {typeof timestamp === 'number' ? (
-          <Typography
-            variant={size === 'small' ? 'body2' : 'body1'}
-            color='textSecondary'
+    <Tooltip content={tooltip} placement='top'>
+      <Stack direction='column' spacing={1} sx={{ ...sx }} {...rest}>
+        <Stack direction='row' spacing={2} sx={{ alignItems: 'center' }}>
+          <LabeledStatusLight
+            status={status}
+            color={segment ? color : 'textSecondary'}
+            size={size}
+            sx={{ flex: 1 }}
           >
-            <TimeAgo date={timestamp} formatter={shortTimeAgoFormatter} />
-          </Typography>
-        ) : null}
+            {title}
+          </LabeledStatusLight>
+          {typeof timestamp === 'number' ? (
+            <Typography
+              variant={size === 'small' ? 'body2' : 'body1'}
+              color='textSecondary'
+            >
+              <TimeAgo date={timestamp} formatter={shortTimeAgoFormatter} />
+            </Typography>
+          ) : null}
+        </Stack>
+
+        {/* key of <LinearProgress /> ensures that the progress bar is not animated */}
+        {/* back to zero when a new stage starts but jumps back abruptly */}
+
+        {segment ? (
+          <LinearProgress
+            key={`${segment.type}-${segment.startMs}`}
+            value={progress}
+            variant='determinate'
+            color={stage === 'completed' ? 'success' : 'primary'}
+            sx={{ height: 4 }}
+          />
+        ) : (
+          <Box sx={{ height: 4, color: colorForStatus(status) }} />
+        )}
       </Stack>
-
-      {/* key of <LinearProgress /> ensures that the progress bar is not animated */}
-      {/* back to zero when a new stage starts but jumps back abruptly */}
-
-      <LinearProgress
-        key={`${segment.type}-${segment.startMs}`}
-        value={progress}
-        variant='determinate'
-        color={stage === 'completed' ? 'success' : 'primary'}
-      />
-    </Stack>
+    </Tooltip>
   );
 };
 
