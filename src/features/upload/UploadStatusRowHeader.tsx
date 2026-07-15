c33fc4a5134f0cd14459091034108c70cd404106
Peter Volf <do.volfp@gmail.com>
2026-07-15 15:30:52 +0200

    refactor: convert UploadStatusLights and UploadStatusRowHeader to TS

--- src/features/upload/UploadStatusRowHeader.tsx
+++ src/features/upload/UploadStatusRowHeader.tsx
@@ -1,6 +1,6 @@
 import Box from '@mui/material/Box';
 import clsx from 'clsx';
-import PropTypes from 'prop-types';
+import type { ReactNode } from 'react';
 
 import { makeStyles } from '@skybrush/app-theme-mui';
 
@@ -11,10 +11,6 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.text.secondary,
   },
 
-  hover: {
-    backgroundColor: theme.palette.action.hover,
-  },
-
   selectable: {
     cursor: 'pointer',
     '&:hover': {
@@ -23,25 +19,28 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const UploadStatusRowHeader = ({ label, onClick, uavIds, ...rest }) => {
+type UploadStatusRowHeaderProps = {
+  label: ReactNode;
+  onClick: (uavIds: string[]) => void;
+  uavIds: string[];
+};
+
+const UploadStatusRowHeader = ({
+  label,
+  onClick,
+  uavIds,
+}: UploadStatusRowHeaderProps) => {
   const classes = useStyles();
-  const clickHandler = onClick && uavIds ? () => onClick(uavIds) : null;
+  const clickHandler = uavIds ? () => onClick(uavIds) : undefined;
 
   return (
     <Box
       className={clsx(classes.root, clickHandler && classes.selectable)}
       onClick={clickHandler}
-      {...rest}
     >
       {label}
     </Box>
   );
-};
-
-UploadStatusRowHeader.propTypes = {
-  label: PropTypes.node,
-  onClick: PropTypes.func,
-  uavIds: PropTypes.arrayOf(PropTypes.string),
 };
 
 export default UploadStatusRowHeader;
